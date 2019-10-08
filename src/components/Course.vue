588591fc8f43d9e1436227273610c72e5b8c15ca
Will Spencer <wes229@cornell.edu>
2019-10-08 00:38:35 -0400

    Basic Course Implementation
    Start scaffolding a course implementation with basic styling, as well as a basic semester layout.

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -1,7 +1,68 @@
 <template>
-  <div></div>
+  <div class = "course">
+    <div class = "course-code">{{subject}} {{ code }}</div>
+    <div class = "course-name">{{ name }}</div>
+    <div class = "course-buttons">
+      <a class = "course-button" :href = "review">
+        <img class="course-image" src = "@/assets/cornell-logo.webp">
+      </a>
+      <a class = "course-button" :href = "roster">
+        <img class="course-image" src = "@/assets/dti-logo.png">
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { Component, Vue} from "vue-property-decorator";
+
+export default {
+  props: {
+    subject: String,
+    code: Number, 
+    name: String,
+  },
+  computed: {
+    review() {
+      return `https://www.cureviews.org/course/${this.subject}/${this.code}`;
+    }, 
+    // TODO: change semester from FA18
+    roster() {
+      return `https://classes.cornell.edu/browse/roster/FA18/class/${this.subject}/${this.code}`;
+    }
+  }
+};
 </script>
+
+
+<style scoped lang="scss">
+.course
+{
+  height: 8.5rem;
+  width: 10rem;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border: 1px black solid;
+  background-color: white;
+  padding: .5rem;
+
+  &-code {
+    font-weight: bold;
+  }
+
+  &-name {
+    margin-top: .5rem;
+  }
+
+  &-image {
+    width: 30px;
+  }
+
+  &-buttons {
+    display: flex;
+    justify-content: space-around;
+    margin: .5rem;
+  }
+}
+</style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,71 @@
 <template>
-  <div></div>
+  <div id="dashboard">
+    <div class="semester">
+      <div class="semester-title">Fall 2019</div>
+      <div class="semester-list">
+        <course class = "semester-class" subject="CS" code="2110" name="Object-Oriented Programming and Data Structures"></course>
+      </div>
+    </div>
+    <div class="semester">
+      <div class="semester-title">Spring 2020</div>
+      <div class="semester-list">
+        <course class = "semester-class" subject="CS" code="2110" name="Object-Oriented Programming and Data Structures"></course>
+        <course class = "semester-class" subject="CS" code="1110" name="Introduction to Computing Using Python"></course>
+      </div>
+    </div>
+    <div class="semester"></div>
+    <div class="semester"></div>
+    <div class="semester"></div>
+    <div class="semester"></div>
+    <div class="semester"></div>
+    <div class="semester"></div>
+  </div>
 </template>
 
 <script>
+import { Component, Vue} from "vue-property-decorator";
+import Course from '@/components/Course';
+
+Vue.component('course', Course);
+
 export default {};
 </script>
+
+<style scoped lang="scss">
+#dashboard
+{
+  margin: 1rem;
+}
+
+.semester
+{
+  margin: 1rem;
+  padding: 1rem;
+  height: 12.12rem;
+
+  &:nth-child(even) {
+    background: lightblue;
+  }
+
+  &:nth-child(odd) {
+    background: lightpink;
+  }
+
+  &-title {
+    font-weight: bold;
+  }
+
+  &-list
+  {
+    margin-top: .5rem;
+    display: flex;
+    flex-direction: row;
+  }
+
+  &-class
+  {
+    margin-right: .5rem;
+  }
+}
+
+</style>
