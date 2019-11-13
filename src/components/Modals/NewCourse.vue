50bde629af867a2921b33377a35e171805e8a0de
Han Wang <hyw2@cornell.edu>
2019-11-12 23:04:11 -0500

    Add courses search

--- src/components/Modals/NewCourse.vue
+++ src/components/Modals/NewCourse.vue
@@ -19,6 +19,7 @@
 
 <script>
 import Vue from 'vue';
+import courses from '../../assets/courses/courses.json'
 
 export default {
   computed: {
@@ -39,42 +40,58 @@ export default {
     }
   },
   mounted: function () {
-    let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-    this.autocomplete(document.getElementsByClassName("newCourse-dropdown")[0], countries);
+    this.autocomplete(document.getElementsByClassName("newCourse-dropdown")[0], courses);
   },
   methods: {
     closeCourseModal: function (event) {
       let modal = document.getElementsByClassName("semester-newCourse")[0];
       modal.style.display = "none";
     },
-    autocomplete(inp, arr) {
+    autocomplete(inp, courses) {
       /*the autocomplete function takes two arguments,
-      the text field element and an array of possible autocompleted values:*/
+      @inp: input
+      @courses: object of courses from JSON
+      */
       var currentFocus;
       /*execute a function when someone writes in the text field:*/
       inp.addEventListener("input", function(e) {
-          var a, b, i, val = this.value;
+          var a, b, i, val = this.value.toUpperCase();
           /*close any already open lists of autocompleted values*/
           closeAllLists();
-          if (!val) { return false;}
+          if (!val) return false;
           currentFocus = -1;
           /*create a DIV element that will contain the items (values):*/
-          a = document.createElement("DIV");
-          a.setAttribute("id", this.id + "autocomplete-list");
-          a.setAttribute("class", "autocomplete-items");
-          /*append the DIV element as a child of the autocomplete container:*/
-          this.parentNode.appendChild(a);
-          /*for each item in the array...*/
-          for (i = 0; i < arr.length; i++) {
-            /*check if the item starts with the same letters as the text field value:*/
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          if (val.length >= 3) {
+            a = document.createElement("DIV");
+            a.setAttribute("id", this.id + "autocomplete-list");
+            a.setAttribute("class", "autocomplete-items");
+            /*append the DIV element as a child of the autocomplete container:*/
+            this.parentNode.appendChild(a);
+
+            /*code array for results that contain course code and title array for results that contain title*/
+            let code = [], title = [];
+            for (let attr in courses) {
+              if (~attr.toUpperCase().indexOf(val)) {
+                code.push(courses[attr].title);
+              }
+              else if (courses[attr].title && ~courses[attr].title.toUpperCase().indexOf(val)) {
+                  title.push(courses[attr].title);
+              }
+            }
+            code.sort();
+            title.sort();
+
+            /*prioritize code matches over title matches*/
+            let match = code.concat(title);
+
+            match.map(title => {
+              /*check if the item starts with the same letters as the text field value:*/
               /*create a DIV element for each matching element:*/
               b = document.createElement("DIV");
               /*make the matching letters bold:*/
-              b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-              b.innerHTML += arr[i].substr(val.length);
+              b.innerHTML = title;
               /*insert a input field that will hold the current array item's value:*/
-              b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+              b.innerHTML += "<input type='hidden' value='" + title + "'>";
               /*execute a function when someone clicks on the item value (DIV element):*/
                   b.addEventListener("click", function(e) {
                   /*insert the value for the autocomplete text field:*/
@@ -84,7 +101,7 @@ export default {
                   closeAllLists();
               });
               a.appendChild(b);
-            }
+            })
           }
       });
       /*execute a function presses a key on the keyboard:*/
