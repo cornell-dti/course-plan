307c78a4c3e13445500fb7900b14ac69fa4b04dd
Will Spencer <will.e.spencer@gmail.com>
2019-11-21 16:57:05 -0500

    Create a Confirmation.vue for when adding a course/semester

---
+++ src/components/Confirmation.vue
@@ -0,0 +1,3 @@
+// One prop for text
+// Otherwise - the same for courses and semesters
+// Can leave as display: none like the 2 modals in Semester.vue until necessary? Or can itself be created from the addCourse() method - unsure what the right move is right now
