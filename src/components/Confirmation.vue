b506adf0471f2c39c96646703d0bd6a0e8f34df0
Theresa Cho <tsc82@cornell.edu>
2019-11-21 17:27:18 -0500

    added confirmation component

--- src/components/Confirmation.vue
+++ src/components/Confirmation.vue
@@ -1,3 +1,21 @@
 // One prop for text
 // Otherwise - the same for courses and semesters
 // Can leave as display: none like the 2 modals in Semester.vue until necessary? Or can itself be created from the addCourse() method - unsure what the right move is right now
+
+<template>
+  <div class="confirmation">
+    <div class="confirmation-text">{{text}}</div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+// TODO: gray out initial dropdown options
+export default {
+    props: {
+        text: String
+    }
+
+};
+</script>
