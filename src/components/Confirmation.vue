5f91dcb6f6ec2cfcf79e9dfe3f5625de8e56185c
Theresa Cho <tsc82@cornell.edu>
2019-11-22 15:37:21 -0500

    confirmation component created

--- src/components/Confirmation.vue
+++ src/components/Confirmation.vue
@@ -4,18 +4,119 @@
 
 <template>
   <div class="confirmation">
+    <div class="confirmation-left"><img class="confirmation-icon" src="../assets/images/checkmark.svg" /></div>
     <div class="confirmation-text">{{text}}</div>
+    <div class="confirmation-right">
+        <span class="undo-buttonText">{{ undo }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
 
-// TODO: gray out initial dropdown options
+
 export default {
     props: {
         text: String
+    },
+
+    computed:{
+        undo() {
+            return 'UNDO';
+        },
     }
+
 
 };
 </script>
+
+<style scoped lang="scss">
+.confirmation{
+    position: absolute;
+    width: 410px;
+    height: 41px;
+    left: 295px;
+    top: 16px;
+    background: #FFFFFF;
+    border-radius: 6px;
+    display: flex;
+    flex-direction: row;
+
+    &-left {
+        // position: absolute;
+        width: 43px;
+        height: 41px;
+        left: 295px;
+        top: 16px;
+
+        background: #3EB53B;
+        border-radius: 6px;
+    }
+    
+    &-left {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    &-left img {
+        // position: absolute;
+        width: 20px;
+        height: 20px;
+        left: 307px;
+        top: 27px;
+
+        margin-left: 12px;
+        margin-right: 12px;
+        margin-top: 11px;
+        margin-bottom: 11px;
+    }
+
+    &-text {
+        width: 274px;
+        height: 16px;
+        left: 349px;
+        top: 29px;
+
+        font-family: Helvetica Neue;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 19px;
+        display: flex;
+        align-items: center;
+
+        color: #7B7D7E;
+
+        margin-left: 11px;
+        margin-right: 19px;
+        margin-top: 13px;
+        margin-bottom: 12px;
+    }
+
+    &-right{
+        // position: absolute;
+        width: 49px;
+        height: 16px;
+        left: 642px;
+        top: 29px;
+
+        font-family: Helvetica Neue;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 20px;
+        display: flex;
+        align-items: center;
+
+        color: #32A0F2;
+        margin-left: 19px;
+        margin-right: 14px;
+        margin-top: 13px;
+        margin-bottom: 12px;
+
+    }
+
+}
+</style>
