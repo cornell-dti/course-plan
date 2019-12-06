f2e3097889206b7a02b853b3bbba3bf0e36f70d9
Theresa Cho <tsc82@cornell.edu>
2019-12-06 09:46:37 -0500

    add sem modal styled

--- src/components/Modals/NewSemester.vue
+++ src/components/Modals/NewSemester.vue
@@ -2,25 +2,38 @@
   <div class="newSemester">
     <div class="newSemester-section newSemester-type">
       <label class="newSemester-label" for="type">{{ typeText }}</label>
-      <select class="newSemester-select" :placeholder="typePlaceholder" id="type">
-        <option value="" selected disabled>Select one</option>
-        <option value="fall">
-          🍂 Fall
-        </option>
-        <option value="spring">
-          🌸 Spring
-        </option>
-        <option value="summer">
-          ☀️ Summer
-        </option>
-        <option value="winter">
-          ❄️ Winter
-        </option>
-      </select>
+      <div class="newSemester-select" id="type">
+        <div class="newSemester-dropdown-placeholder season-wrapper" v-on:click="showHideSeasonContent">
+          <div class="newSemester-dropdown-placeholder season-placeholder" id="season-placeholder">{{seasonPlaceholder}}</div>
+          <div class="newSemester-dropdown-placeholder season-arrow" id="season-arrow"></div>
+        </div>
+        <!-- <input class="newSemester-dropdown-placeholder" placeholder='Select one'/> -->
+        <div class="newSemester-dropdown-content season-content" id="season-content">
+          <div class="newSemester-dropdown-content-item" id="fall" v-on:click="selectSeason('fall')">
+            🍂 Fall
+          </div>
+          <div class="newSemester-dropdown-content-item" id="spring" v-on:click="selectSeason('spring')">
+            🌸 Spring
+          </div>
+          <div class="newSemester-dropdown-content-item" id="summer" v-on:click="selectSeason('summer')">
+            ☀️ Summer
+          </div>
+          <div class="newSemester-dropdown-content-item" id="winter" v-on:click="selectSeason('winter')">
+            ❄️ Winter
+          </div>
+        </div>
+      </div>
     </div>
     <div class="newSemester-section newSemester-year">
       <label class="newSemester-label" for="year">{{ yearText }}</label>
-      <select class="newSemester-select" id="year" v-html="yearOptions" />
+      <div class="newSemester-select" id="year">
+        <!-- <input class="newSemester-dropdown-placeholder" :placeholder="currentYear"/> -->
+        <div class="newSemester-dropdown-placeholder year-wrapper" v-on:click="showHideYearContent">
+          <div class="newSemester-dropdown-placeholder year-placeholder" id="year-placeholder">{{ yearPlaceholder }}</div>
+          <div class="newSemester-dropdown-placeholder year-arrow" id="year-arrow"></div>
+        </div>
+        <div class="newSemester-dropdown-content year-content" v-html="yearOptions" id="year-content"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,15 +41,18 @@
 <script>
 // TODO: gray out initial dropdown options
 export default {
+  data: function() {
+    return {
+      seasonPlaceholder: "Select One",
+      yearPlaceholder: (new Date()).getFullYear()
+    };
+  },
   computed: {
     typeText() {
       return 'Type';
     },
     yearText() {
       return 'Year';
-    },
-    typePlaceholder() {
-      return 'Select one';
     },
     yearOptions() {
       // TODO: what years are valid?
@@ -52,13 +68,77 @@ export default {
       // const years = ['2019', '2020', '2021', '2022'];
       let str = '';
       for (let i = 0; i < years.length; i += 1) {
-        if (years[i] === currentYear) {
-          str += `<option value=${years[i]} selected>${years[i]}</option>`;
-        } else {
-          str += `<option value=${years[i]}>${years[i]}</option>`;
-        }
+        str += `<div class="newSemester-dropdown-content-item" id=${years[i]} v-on:click="selectYear(${years[i]})">${years[i]}</div>`;
+
       }
       return str;
+    }
+  },
+  methods: {
+    showHideSeasonContent() {
+      const season_content = document.getElementById('season-content');
+
+      const content_shown = season_content.style.display == 'block';
+      
+      if (content_shown){ //clicked box when content shown. So then hide content
+        season_content.style.display = 'none';
+      }
+
+      else{ //show season content
+      season_content.style.display = 'block';
+      }
+
+      //change input box styling
+      const season_box = document.getElementById('type');
+      season_box.style.borderColor = '#32A0F2';
+
+      const season_arrow = document.getElementById('season-arrow');
+      season_arrow.style.borderTopColor = '#32A0F2';
+    },
+    showHideYearContent() {
+      const year_content = document.getElementById('year-content');
+      
+      const content_shown = year_content.style.display == 'block';
+      
+      if (content_shown){ //clicked box when content shown. So then hide content
+        year_content.style.display = 'none';
+      }
+
+      else{ //show year_content 
+      year_content.style.display = 'block';
+      }
+
+      //change input box styling
+      const year_box = document.getElementById('year');
+      year_box.style.borderColor = '#32A0F2';
+
+      const year_arrow = document.getElementById('year-arrow');
+      year_arrow.style.borderTopColor = '#32A0F2';
+    },
+    selectSeason(selected_id){
+      const selected_season = document.getElementById(selected_id);
+
+      this.seasonPlaceholder = selected_season.innerText;
+
+      const season_arrow = document.getElementById('season-arrow');
+      season_arrow.style.borderTopColor = '#C4C4C4';
+
+      const season_placeholder_text = document.getElementById('season-placeholder');
+      season_placeholder_text.style.color = '#757575'; 
+
+      const season_content = document.getElementById('season-content');
+      season_content.style.display = 'none';
+
+    },
+    selectYear(selected_id){
+      const selected_year = document.getElementById(selected_id);
+
+      console.log(selected_year.innerText);
+
+      this.yearPlaceholder = selected_year.innerText;
+
+      const year_arrow = document.getElementById('year-arrow');
+      year_arrow.style.borderTopColor = '#C4C4C4';
     }
   }
 };
@@ -96,8 +176,13 @@ export default {
     left: 444px;
     top: 183px;
 
-    background: #ffffff;
-    border: 1px solid #32a0f2;
+    background: #FFFFFF;
+    // border: 1px solid #32A0F2;
+
+    border: 1px solid #C4C4C4;
+
+    //when selected border-color: #32A0F2;
+
     box-sizing: border-box;
     border-radius: 1px;
     width: 100%;
@@ -110,10 +195,136 @@ export default {
     color: #b6b6b6;
   }
 
+
+
   &-icon {
     width: 12px;
     height: 12px;
   }
+
+  &-dropdown {
+    &-placeholder {
+      font-family: Helvetica Neue;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 17px;
+
+      color: #B6B6B6;
+
+      background-color: white;
+      color: #B6B6B6;
+
+      &.season-wrapper, &.year-wrapper {
+        display: flex;
+        flex-direction: row;
+        // width: 121px;
+        // height: 16px;
+        width: 100%;
+        height: 100%;
+      }
+
+      &.year-wrapper {
+        // width: 62px;
+        // height: 16px;
+        width: 100%;
+        height: 100%;
+      }
+
+      &.season-placeholder, &.year-placeholder {
+        margin-left: 7px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        width: 70%;
+      }
+
+
+      &.season-arrow, &.year-arrow {
+          width: 6.24px;
+          height: 6.24px;
+          border-left: 6.24px solid transparent;
+          border-right: 6.24px solid transparent;
+          
+          border-top: 6.24px solid #C4C4C4;
+          
+          //when clicked border-top-color: #32A0F2;
+
+          margin-top: 10.17px;
+          margin-bottom: 10.17px;
+
+          margin-right: 8.7px;
+          margin-left: 5px;
+
+
+
+      }
+
+      &.year-arrow {
+        margin-right: 9.17px;
+        margin-left: 17px;
+      }
+
+      &.year-placeholder {
+        width: 62px;
+        height: 16px;
+        left: 581px;
+        top: 188px;
+      }
+
+    }
+  }
+
+  &-dropdown-content{
+    display: none;
+    width: 114px;
+    height: 134px;
+    left: 444px;
+
+    background: #FFFFFF;
+    box-shadow: -4px 4px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 7px;
+
+    margin-top: 3px;
+
+    &.year-content{
+      width: 114px;
+      height: 223px;
+      left: 574px;
+      top: 209px;
+
+      background: #FFFFFF;
+      box-shadow: -4px 4px 10px rgba(0, 0, 0, 0.25);
+      border-radius: 7px;
+
+      overflow: scroll;
+    }
+
+    &-item{
+      width: 106px;
+      height: 31px;
+      left: 454px;
+      top: 213px;
+
+      font-family: Helvetica Neue;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 17px;
+      display: flex;
+      align-items: center;
+
+      color: #757575;
+
+      padding-left: 10px;
+    }
+  }
+
+  &-dropdown-content div:hover{
+    background: rgba(50, 160, 242, 0.15);
+    width: 100%;
+  }
+
+
 }
 
 select option {
