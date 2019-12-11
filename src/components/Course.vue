26c67043304b79fec52deb7d26e4380745556062
boonakij <boonakij@gmail.com>
2019-12-10 19:31:24 -0500

    click 3 dots works now

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -51,7 +51,7 @@
         </div>
       </div>
     </div>
-    <!-- <coursemenu v-if="menuOpen" class="course-menu" v-click-outside="closeMenuIfOpen"/> -->
+    <coursemenu v-if="menuOpen" class="course-menu" v-click-outside="closeMenuIfOpen"/>
     <coursemenu v-if="menuOpen" class="course-menu"/>
   </div>
 </template>
@@ -89,6 +89,7 @@ export default {
   data() {
     return {
       menuOpen: false,
+      stopCloseFlag: false
     }
   },
   computed: {
@@ -189,11 +190,17 @@ export default {
   },
   methods: {
     openMenu() {
+      this.stopCloseFlag = true;
       this.menuOpen = true;
     },
     closeMenuIfOpen() {
-      if(this.menuOpen) {
-        this.menuOpen = false;
+      if(this.stopCloseFlag) {
+        this.stopCloseFlag = false;
+      }
+      else {
+        if(this.menuOpen) {
+          this.menuOpen = false;
+        }
       }
     }
   },
@@ -282,7 +289,7 @@ export default {
       height: 5px;
       background-color: #c4c4c4;
       opacity: 1;
-      margin: 0 2px; 
+      margin: 0 2px;
     }
   }
 
