eb9a45c1cc9eace764733f281fc77ff18edf6d05
Han Wang <hyw2@cornell.edu>
2020-02-08 16:28:13 -0500

    Fix lint errors

--- src/components/Modals/NewCourse.vue
+++ src/components/Modals/NewCourse.vue
@@ -142,7 +142,7 @@ export default {
         if (currentFocus >= x.length) currentFocus = 0;
         if (currentFocus < 0) currentFocus = x.length - 1;
         /* add class "autocomplete-active": */
-        x[currentFocus].classList.add('autocomplete-active');        
+        x[currentFocus].classList.add('autocomplete-active');
       }
       function removeActive(x) {
         /* a function to remove the "active" class from all autocomplete items: */
