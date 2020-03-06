f404a0f90f031633b5b9ba94c9a30effbe0a0899
Rose Zhou <rmz38@cornell.edu>
2020-03-06 15:30:20 -0500

    Finished 404 page by replacing absolute positioning css with flex

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,129 +1,53 @@
 <template>
-<html>
-  <head>
-    <link rel="stylesheet" href="dti_404.css" />
-  </head>
-  <body>
-    <svg
-      id="ccplan"
-      width="110"
-      height="45"
-      viewBox="0 0 110 45"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M7.56036 14.9225C10.8437 14.9225 12.6582 13.1707 13.6951 11.3757L11.0597 10.0997C10.4549 11.2892 9.15883 12.1975 7.56036 12.1975C5.05464 12.1975 3.15375 10.1862 3.15375 7.46125C3.15375 4.73627 5.05464 2.72498 7.56036 2.72498C9.15883 2.72498 10.4549 3.65493 11.0597 4.82278L13.6951 3.52517C12.6798 1.73015 10.8437 0 7.56036 0C3.34816 0 0 3.02775 0 7.46125C0 11.8948 3.34816 14.9225 7.56036 14.9225Z"
-        fill="black"
-      />
-      <path
-        d="M20.2947 14.9225C23.7508 14.9225 25.8245 12.4138 25.8245 9.42929C25.8245 6.46642 23.7508 3.95771 20.2947 3.95771C16.8601 3.95771 14.7864 6.46642 14.7864 9.42929C14.7864 12.4138 16.8601 14.9225 20.2947 14.9225ZM20.2947 12.4787C18.5882 12.4787 17.6377 11.0729 17.6377 9.42929C17.6377 7.80728 18.5882 6.40154 20.2947 6.40154C22.0011 6.40154 22.9732 7.80728 22.9732 9.42929C22.9732 11.0729 22.0011 12.4787 20.2947 12.4787Z"
-        fill="black"
-      />
-      <path
-        d="M27.8597 14.663H30.6031V7.76403C31.0567 7.0936 32.2664 6.57455 33.1736 6.57455C33.476 6.57455 33.7352 6.61781 33.9296 6.66106V3.97933C32.6336 3.97933 31.3375 4.71465 30.6031 5.6446V4.21723H27.8597V14.663Z"
-        fill="black"
-      />
-      <path
-        d="M42.6856 14.663H45.429V7.28824C45.429 5.25532 44.3273 3.95771 42.0376 3.95771C40.3311 3.95771 39.0567 4.77953 38.387 5.57972V4.21723H35.6437V14.663H38.387V7.63427C38.8407 7.00709 39.6831 6.40154 40.7632 6.40154C41.9296 6.40154 42.6856 6.89895 42.6856 8.34795V14.663Z"
-        fill="black"
-      />
-      <path
-        d="M47.4833 9.42929C47.4833 12.7815 49.881 14.9225 53.0563 14.9225C54.6764 14.9225 56.3181 14.4251 57.3549 13.4735L56.1453 11.7001C55.4756 12.3489 54.3308 12.7382 53.3587 12.7382C51.5875 12.7382 50.529 11.6352 50.3562 10.316H58.111V9.71044C58.111 6.2934 55.9941 3.95771 52.8835 3.95771C49.7082 3.95771 47.4833 6.40154 47.4833 9.42929ZM52.8835 6.14202C54.698 6.14202 55.3892 7.46125 55.454 8.45609H50.313C50.4426 7.418 51.177 6.14202 52.8835 6.14202Z"
-        fill="black"
-      />
-      <path d="M60.0925 14.663H62.8359V0.237895H60.0925V14.663Z" fill="black" />
-      <path d="M65.5561 14.663H68.2994V0.237895H65.5561V14.663Z" fill="black" />
-      <path
-        d="M7.56036 41.2802C10.8437 41.2802 12.6582 39.5284 13.6951 37.7334L11.0597 36.4574C10.4549 37.6469 9.15883 38.5552 7.56036 38.5552C5.05464 38.5552 3.15375 36.5439 3.15375 33.8189C3.15375 31.094 5.05464 29.0827 7.56036 29.0827C9.15883 29.0827 10.4549 30.0126 11.0597 31.1805L13.6951 29.8829C12.6798 28.0878 10.8437 26.3577 7.56036 26.3577C3.34816 26.3577 0 29.3854 0 33.8189C0 38.2524 3.34816 41.2802 7.56036 41.2802Z"
-        fill="black"
-      />
-      <path
-        d="M20.2947 41.2802C23.7508 41.2802 25.8245 38.7715 25.8245 35.787C25.8245 32.8241 23.7508 30.3154 20.2947 30.3154C16.8601 30.3154 14.7864 32.8241 14.7864 35.787C14.7864 38.7715 16.8601 41.2802 20.2947 41.2802ZM20.2947 38.8364C18.5882 38.8364 17.6377 37.4306 17.6377 35.787C17.6377 34.165 18.5882 32.7592 20.2947 32.7592C22.0011 32.7592 22.9732 34.165 22.9732 35.787C22.9732 37.4306 22.0011 38.8364 20.2947 38.8364Z"
-        fill="black"
-      />
-      <path
-        d="M34.9017 41.0207H37.645V30.5749H34.9017V37.6469C34.4265 38.2524 33.584 38.8364 32.5256 38.8364C31.3591 38.8364 30.6031 38.3606 30.6031 36.9116V30.5749H27.8597V37.9929C27.8597 40.0258 28.9398 41.2802 31.2295 41.2802C32.936 41.2802 34.1889 40.5016 34.9017 39.7014V41.0207Z"
-        fill="black"
-      />
-      <path
-        d="M40.3689 41.0207H43.1123V34.1217C43.5659 33.4513 44.7755 32.9322 45.6828 32.9322C45.9852 32.9322 46.2444 32.9755 46.4388 33.0187V30.337C45.1428 30.337 43.8467 31.0723 43.1123 32.0023V30.5749H40.3689V41.0207Z"
-        fill="black"
-      />
-      <path
-        d="M47.2457 39.6582C48.3473 40.6746 50.097 41.2802 51.9115 41.2802C54.806 41.2802 56.4261 39.8312 56.4261 37.928C56.4261 35.3328 54.0284 34.857 52.2355 34.511C51.069 34.2731 50.2482 34.0352 50.2482 33.3864C50.2482 32.7808 50.8962 32.4132 51.8899 32.4132C53.1643 32.4132 54.3524 32.9539 54.9788 33.6243L56.0805 31.7428C55.0436 30.9209 53.6396 30.3154 51.8683 30.3154C49.125 30.3154 47.5913 31.8509 47.5913 33.6027C47.5913 36.0898 49.9026 36.5223 51.6955 36.8683C52.8835 37.1062 53.7908 37.3657 53.7908 38.101C53.7908 38.7499 53.2291 39.1608 52.0411 39.1608C50.745 39.1608 49.1898 38.4471 48.4337 37.7334L47.2457 39.6582Z"
-        fill="black"
-      />
-      <path
-        d="M57.7775 35.787C57.7775 39.1391 60.1752 41.2802 63.3506 41.2802C64.9706 41.2802 66.6123 40.7828 67.6492 39.8312L66.4395 38.0578C65.7699 38.7066 64.625 39.0959 63.653 39.0959C61.8817 39.0959 60.8232 37.9929 60.6504 36.6737H68.4052V36.0681C68.4052 32.6511 66.2883 30.3154 63.1778 30.3154C60.0024 30.3154 57.7775 32.7592 57.7775 35.787ZM63.1778 32.4997C64.9922 32.4997 65.6835 33.8189 65.7483 34.8138H60.6072C60.7368 33.7757 61.4713 32.4997 63.1778 32.4997Z"
-        fill="black"
-      />
-      <path
-        d="M73.1301 45V39.6798C73.9725 40.7611 75.1174 41.2802 76.3486 41.2802C79.0272 41.2802 80.9713 39.2689 80.9713 35.787C80.9713 32.3051 79.0272 30.3154 76.3486 30.3154C75.0742 30.3154 73.9293 30.8777 73.1301 31.8941V30.5749H70.3868V45H73.1301ZM78.1199 35.787C78.1199 37.6036 77.0615 38.8364 75.4846 38.8364C74.599 38.8364 73.5837 38.2957 73.1301 37.6469V33.9487C73.5837 33.2783 74.599 32.7592 75.4846 32.7592C77.0615 32.7592 78.1199 33.992 78.1199 35.787Z"
-        fill="black"
-      />
-      <path d="M83.0225 41.0207H85.7659V26.5956H83.0225V41.0207Z" fill="black" />
-      <path
-        d="M94.7504 41.0207H97.4937V34.2731C97.4937 31.267 95.312 30.3154 92.9359 30.3154C91.2942 30.3154 89.6525 30.8344 88.3781 31.959L89.4149 33.7973C90.3005 32.9755 91.3374 32.5646 92.4607 32.5646C93.8431 32.5646 94.7504 33.2566 94.7504 34.3164V35.7654C94.0591 34.9219 92.8279 34.4894 91.4454 34.4894C89.7821 34.4894 87.8164 35.3761 87.8164 37.8199C87.8164 40.1556 89.7821 41.2802 91.4454 41.2802C92.8063 41.2802 94.0375 40.7828 94.7504 39.9393V41.0207ZM94.7504 38.512C94.2967 39.1175 93.4327 39.4203 92.5471 39.4203C91.467 39.4203 90.5814 38.858 90.5814 37.8848C90.5814 36.8683 91.467 36.306 92.5471 36.306C93.4327 36.306 94.2967 36.6088 94.7504 37.2143V38.512Z"
-        fill="black"
-      />
-      <path
-        d="M107.257 41.0207H110V33.6459C110 31.613 108.898 30.3154 106.609 30.3154C104.902 30.3154 103.628 31.1372 102.958 31.9374V30.5749H100.215V41.0207H102.958V33.992C103.412 33.3648 104.254 32.7592 105.334 32.7592C106.501 32.7592 107.257 33.2566 107.257 34.7056V41.0207Z"
-        fill="black"
-      />
-    </svg>
-    <div id="logo">
-      <div id="first_logo_box" class="color_box"></div>
-      <div id="second_logo_box" class="color_box"></div>
-      <div id="third_logo_box" class="color_box"></div>
-      <div id="fourth_logo_box" class="color_box"></div>
+<div>
+  <div class = "body-container">
+    <!--TOP BAR-->
+    <div class="container p-0">
+        <div class="row top-bar justify-content-between no-gutters">
+            <div class="col-10">
+                <img class="logo" src="@/assets/images/logo.svg" alt = "logo" />
+              </div>
+        </div>
     </div>
-    <div id="band">
-      <div id="first_four">
-        <div id="first_box1" class="color_box"></div>
-        <div id="second_box1" class="color_box"></div>
-        <div id="third_box1" class="color_box"></div>
-        <div id="fourth_box1" class="color_box"></div>
+    <!-- <img data-v-ef68022e="" src="img/logo.4d084161.svg" alt="logo" class="logo"> -->
+    <div class = "message-container">
+      <img class = "img-404" src="@/assets/images/404.svg" alt = "404">
+      <div class = "oops-wrapper">
+        <div id="oops" class="color_box">
+          Oops...
+          <br/>Page Not Found <img class = "thinking-face" src = "@/assets/images/thinking_face.svg">
+        </div>
       </div>
-      <div id="zero">
-        <div id="first_box2" class="color_box"></div>
-        <div id="second_box2" class="color_box"></div>
-        <div id="third_box2" class="color_box"></div>
-        <div id="fourth_box2" class="color_box"></div>
-      </div>
-      <div id="second_four">
-        <div id="first_box3" class="color_box"></div>
-        <div id="second_box3" class="color_box"></div>
-        <div id="third_box3" class="color_box"></div>
-        <div id="fourth_box3" class="color_box"></div>
-      </div>
-      <div id="oops" class="color_box">
-        Oops...
-        <br />Page Not Found 🤔
-      </div>
-      <div id="back_to_home">Back to home</div>
     </div>
-    <div id="bottom_links">
-      <div id="copyright">Cornell DTI @ 2019</div>
-      <div id="love">Built with LOVE</div>
-      <div id="privacy">Privacy Policy</div>
+    <div class="back_to_home">
+      <a class = "back_to_home_link" href = "/login">Back to home</a>
     </div>
-  </body>
-</html>
+    <div class = "footer-container">
+      <div class="container-fluid footer p-0 page_404">
+          <div class= "row footer justify-content-center m-0">
+              <div class="col-3 footer">
+                <!-- <a href ="https://www.cornelldti.org" class= "footer"> Cornell DTI @ </a> -->
+                <a href ="https://www.cornelldti.org" class= "footer"> Cornell DTI @ {{getYear()}}</a>
+              </div>
+              <div class="col-3">
+                  <p class= "footer">Built with ❤️</p>
+              </div>
+              <div class="col-3 footer">
+                  <a  class="footer" href="https://app.termly.io/document/privacy-policy/fcecc0e8-8af2-472d-8d27-b6b89d02a2be">Privacy Policy</a>
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
   methods: {
-    placeholder() {
-      const boxes = document
-        .getElementById('band')
-        .querySelectorAll('.color_box');
-
-      for (let x = 0; x < boxes.length; x += 1) {
-        const posy = parseInt(boxes[x].offsetTop, 10) - 55.16;
-        boxes[x].style.top = posy;
-      }
+    getYear() {
+      const today = new Date();
+      return today.getFullYear();
     }
   }
 };
@@ -131,243 +55,102 @@ export default {
 
 
 <style scoped lang="scss">
-.band {
-  width: 100%;
-  height: 60%;
-}
-.color_box {
-  /* margin: 2px; */
-  display: inline;
-}
-
-/* #first_four{
-    height: 143.32px;
-    display: inline-block;
-} */
-/* start first four */
-#first_box1 {
-  position: absolute;
-  width: 34.72px;
-  height: 72px;
-  left: 226.75px;
-  top: 215.16px;
-  background: #92c3e6;
-  border-radius: 1.3px;
-  transform: skew(-25deg);
-}
-#second_box1 {
-  position: absolute;
-  width: 28px;
-  height: 72px;
-  left: 281.26px;
-  top: 215.16px;
-  background: #1aa9a5;
-}
-#third_box1 {
-  position: absolute;
-  width: 25px;
-  height: 109px;
-  left: 251.75px;
-  top: 249.16px;
-  background: #508197;
-  transform: rotate(90deg);
-}
-#fourth_box1 {
-  position: absolute;
-  width: 28px;
-  height: 23px;
-  left: 281.26px;
-  top: 320.16px;
-  background: #105351;
-}
-/* end first four */
-/* start zero */
-#first_box2 {
-  position: absolute;
-  width: 81.6px;
-  height: 33.6px;
-  left: 352.83;
-  top: 215.16px;
-  background: #92c3e6;
-}
-#second_box2 {
-  position: absolute;
-  width: 33.6px;
-  height: 81.6px;
-  left: 439.23;
-  top: 215.16px;
-  background: #1aa9a5;
-}
-#third_box2 {
-  position: absolute;
-  width: 33.6px;
-  height: 81.6px;
-  left: 352.83;
-  top: 253.56;
-  background: #508197;
-}
-#fourth_box2 {
-  position: absolute;
-  width: 81.6px;
-  height: 33.6px;
-  left: 391.23;
-  top: 301.56;
-  background: #105351;
-}
-/* end zero */
-/* start second four 313.26 is diff*/
-#first_box3 {
-  position: absolute;
-  width: 34.72px;
-  height: 72px;
-  left: 525px;
-  top: 215.16px;
-  background: #92c3e6;
-  border-radius: 1.3px;
-  transform: skew(-25deg);
-}
-#second_box3 {
-  position: absolute;
-  width: 28px;
-  height: 72px;
-  left: 579.52px;
-  top: 215.16px;
-  background: #1aa9a5;
-}
-#third_box3 {
-  position: absolute;
-  width: 25px;
-  height: 109px;
-  left: 550px;
-  top: 249.16px;
-  background: #508197;
-  transform: rotate(90deg);
-}
-#fourth_box3 {
-  position: absolute;
-  width: 28px;
-  height: 23px;
-  left: 579.52px;
-  top: 320.16px;
-  background: #105351;
-}
-/* end second four */
 
 #oops {
-  position: absolute;
-  width: 553px;
-  height: 134px;
-  left: 670px;
-  top: 207px;
-  font-family: Proxima Nova;
-  font-style: normal;
+  text-align: left;
+  display: inline-block;
   font-weight: bold;
-  font-size: 60px;
-  line-height: 73px;
+  font-size: 55px;
   color: #0a2327;
+  vertical-align: top;
+  @media(max-width: 1154px){
+    margin-top: 8%;
+  }
 }
-#back_to_home {
-  /* Back to home */
+.back_to_home {
 
-  position: absolute;
-  width: 280px;
-  height: 43px;
-  left: 614px;
-  top: 410px;
-
-  font-family: Proxima Nova;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 30px;
-  line-height: 43px;
-  /* identical to box height */
-
+  font-size: 35px;
+  vertical-align: middle;
   text-align: center;
+  color: #7b7d7e;
+  margin-bottom: 6vh;
+  margin-top: 6vh;
+  height: 100px;
 
+}
+
+.container {
+  max-width: 100%;
+}
+.page_404{
+  bottom: 0;
+  // margin-top: -50px;
+}
+.top-bar{
+  padding:59px 0px 0px 104px;
+
+  @media (max-width: 1154px) {
+    padding: 59px 104px 0px 104px;
+    display: flex;
+    flex-direction: column;
+  }
+}
+a.back_to_home_link{
   color: #7b7d7e;
 }
-#copyright {
-  position: absolute;
-  width: 220px;
-  height: 24px;
-  left: 377px;
-  top: 550px;
-
-  font-family: Proxima Nova;
+p.footer{
+      text-align: center;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 24px;
+      line-height: 28px;
+      color: #757575;
+    }
+a.footer {
+  text-align: center;
   font-style: normal;
   font-weight: normal;
-  font-size: 20px;
-  line-height: 29px;
-
+  font-size: 24px;
+  line-height: 28px;
   color: #757575;
 }
-#ccplan {
-  position: absolute;
-  width: 110px;
-  height: 45px;
-  left: 100px;
-  top: 34px;
+.col-3 {
+  margin-left: .5rem;
+  margin-right: .5rem;
 }
-#love {
-  position: absolute;
-  width: 181px;
-  height: 29px;
-  left: 664px;
-  top: 550px;
-  font-family: Proxima Nova;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 20px;
-  line-height: 29px;
-  color: #757575;
+.col-3.footer{
+  text-align: center;
 }
-#privacy {
-  position: absolute;
-  width: 164px;
-  height: 29px;
-  left: 920px;
-  top: 550px;
-
-  font-family: Proxima Nova;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 20px;
-  line-height: 29px;
-
-  color: #757575;
+.row.footer{
+  padding : 40px;
 }
-/* start logo */
-#first_logo_box {
-  position: absolute;
-  width: 34px;
-  height: 14px;
-  left: 31px;
-  top: 30px;
-  background: #92c3e6;
+.img-404{
+  min-width: 15vw;
+  vertical-align: top;
+  display: inline-block;
+  @media(max-width: 1154){
+    display:block;
+  }
 }
-#second_logo_box {
-  position: absolute;
-  width: 14px;
-  height: 34px;
-  left: 67px;
-  top: 30px;
-  background: #1aa9a5;
+.oops-wrapper{
+  min-width: 20vw;
+  display: inline-block;
+  margin-left: 5%;
 }
-#third_logo_box {
-  position: absolute;
-  width: 14px;
-  height: 34px;
-  left: 31px;
-  top: 46px;
-  background: #508197;
+.message-container{
+  display: flex;
+  margin-bottom: 3%;
+  margin-top: 5%;
+  justify-content: center;
+  @media(max-width: 1154px){
+    flex-direction: column;
+    align-items: center;
+  }
 }
-#fourth_logo_box {
-  position: absolute;
-  width: 34px;
-  height: 14px;
-  left: 47px;
-  top: 66px;
-  background: #105351;
+.thinking-face{
+  margin-bottom: 1vw;
 }
-/* end logo */
+.body-container{
+  min-height: 100vh;
+}
 </style>
