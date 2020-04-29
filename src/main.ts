75dceab95d07badd39293cb6293c413d21a69ec7
Han Wang <hyw2@cornell.edu>
2020-04-28 23:39:20 -0400

    Add initial config for google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 
 import BootstrapVue from 'bootstrap-vue';
+import VueGtag from 'vue-gtag';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -15,6 +16,10 @@ import * as fb from './firebaseConfig';
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-124837875-2' }
+});
 
 // handle page reloads
 let app: Vue | undefined;
