c205c032fb304fba9c38adf2418dd638a0f47a85
Theresa <tsc82@cornell.edu>
2020-10-23 15:22:45 -0400

    Split filtered-all-courses.json into semesters and added fields
    - Added enrollGroups, catalogWhenOffered, catalogPrereqCoreq fields
    - Split filtered-all-courses.json into semester jsons
    - New exported filtered-all-courses does not have in-order semester rosters

--- src/requirements/filtered-all-courses.ts
+++ src/requirements/filtered-all-courses.ts
@@ -2,17 +2,25 @@
  * The module that re-exposes all jsons with strict static types.
  */
 
-import { readFileSync } from 'fs';
+import { readFileSync, readdirSync } from 'fs';
 import { Course } from './types';
+
+
+/** All paths to the filtered-<semester name>-courses.json files. */
+const filteredCoursesPaths: string[] = readdirSync('src/requirements/').filter(
+  path => path.includes('filtered') && path.includes('courses.json')
+);
 
 // NOTE:
 //   We need to use the type cast trick here,
 //   because TypeScript has limited capability to correctly infer types of deeply nested json with enums.
 
 /** All courses fetch from Cornell Class API, with only the fields we need. */
-const filteredAllCourses: { readonly [semester: string]: readonly Course[] } = JSON.parse(
-  // Need to read from file. It will take TS forever to type checker this giant json.
-  readFileSync('src/requirements/filtered-all-courses.json').toString()
-);
+const filteredAllCourses: { readonly [semester: string]: readonly Course[] } =
+  // Read and parse each JSON file using type cast trick.
+  // Reduce to one JSON object
+  filteredCoursesPaths.map(path => JSON.parse(readFileSync('src/requirements/' + path).toString()))
+                      .reduce((accum, currentValue) => Object.assign(accum, currentValue));
+
 
 export default filteredAllCourses;
