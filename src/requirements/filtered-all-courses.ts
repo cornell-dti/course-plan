f1adc12e906a76ee4d63887086e7bdebecb1dd9f
Theresa <tsc82@cornell.edu>
2020-10-23 16:19:58 -0400

    Modified filtered-all-courses.ts to combine semester course data files in order and uploading old decorated-requirements.json for testing purposes

--- src/requirements/filtered-all-courses.ts
+++ src/requirements/filtered-all-courses.ts
@@ -6,9 +6,60 @@ import { readFileSync, readdirSync } from 'fs';
 import { Course } from './types';
 
 
-/** All paths to the filtered-<semester name>-courses.json files. */
-const filteredCoursesPaths: string[] = readdirSync('src/requirements/').filter(
-  path => path.includes('filtered') && path.includes('courses.json')
+function typeToOrderedNumber(type: string): number {
+  switch(type) {
+    case 'WI':
+      return 0;
+    case 'SP':
+      return 1;
+    case 'SU':
+      return 2;
+    case 'FA':
+      return 3;
+    default:
+      return 4;
+  }
+}
+
+function compareFilteredCoursesPaths(filteredCoursesPath1: string, filteredCoursesPath2: string): number {
+  // Extract just the roster part in the filteredCoursesPath
+  const roster1 = filteredCoursesPath1.split('-')[1];
+  const roster2 = filteredCoursesPath2.split('-')[1];
+  let type1 = roster1.slice(0,2);
+  let year1 = roster1.slice(2);
+  let type2 = roster2.slice(0, 2);
+  let year2 = roster2.slice(2);
+
+  if (year1 < year2) {
+    // roster1 has less recent year than roster2
+    return -1;
+  } else if (year2 < year1) {
+    // roster2 has less recent year than roster1
+    return 1;
+  } else if (typeToOrderedNumber(type1) < typeToOrderedNumber(type2)) {
+    // roster1 has less recent semester type than roster2
+    return -1;
+  } else if (typeToOrderedNumber(type2) < typeToOrderedNumber(type1)) {
+    // roster2 has less recent semester type than roster1
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function sortByLeastRecentRosters(filteredCoursesPaths: string[]): string[] {
+  // Sorts from least recent roster
+  return filteredCoursesPaths.sort(compareFilteredCoursesPaths);
+}
+
+
+/** All paths to the filtered-<semester name>-courses.json files.
+ * Sorted by least recent rosters
+ */
+const filteredCoursesPaths: string[] = sortByLeastRecentRosters(
+  readdirSync('src/requirements/').filter(
+    path => path.includes('filtered') && path.includes('courses.json')
+  )
 );
 
 // NOTE:
