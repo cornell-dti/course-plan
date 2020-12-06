54d614d93b31b4667fdea5a8c2ac74aca046b2ac
Benjamin Shen <bfs45@cornell.edu>
2020-12-05 19:11:47 -0500

    Tweak logic, add ap credit

--- src/requirements/data/exams/ExamCredit.ts
+++ src/requirements/data/exams/ExamCredit.ts
@@ -4,7 +4,7 @@ import { CourseTaken } from '../../types';
 export type ExamRequirements = {
   readonly name: string;
   readonly fulfillment: {
-    readonly courseEquivalents: Record<string, number>;
+    readonly courseEquivalents?: Record<string, number | number[]>;
     readonly minimumScore: number;
     readonly credits: number;
     readonly majorsExcluded?: string[];
@@ -42,17 +42,29 @@ function userDataToCourses(
     // generate the equivalent course
     if (exam) {
       const roster = 'FA20'; // TODO this is hardcoded
-      const courseId = exam.fulfillment.courseEquivalents[college];
+      const courseEquivalents =
+        (exam.fulfillment.courseEquivalents &&
+          (exam.fulfillment.courseEquivalents[college] ||
+            exam.fulfillment.courseEquivalents['DEFAULT'])) ||
+        [];
+      let courseIds: number[]; // [A,B,C] => (A and B and C)
+      if (typeof courseEquivalents === 'number') {
+        courseIds = [courseEquivalents];
+      } else {
+        courseIds = courseEquivalents;
+      }
       const excludedMajor =
         exam.fulfillment.majorsExcluded && exam.fulfillment.majorsExcluded.includes(major);
-      if (courseId && !excludedMajor) {
-        courses.push({
-          roster,
-          courseId,
-          code: `${examType} ${exam.name}`,
-          subject: examType,
-          number: exam.name,
-          credits: exam.fulfillment.credits,
+      if (!excludedMajor) {
+        courseIds.forEach(courseId => {
+          courses.push({
+            roster,
+            courseId,
+            code: `${examType} ${exam.name}`,
+            subject: examType,
+            number: exam.name,
+            credits: exam.fulfillment.credits,
+          });
         });
       }
     }
@@ -91,27 +103,245 @@ export default function getCourseEquivalentsFromUserExams(user: AppUser): readon
 export const examData: ExamData = {
   AP: [
     {
-      name: 'Computer Science A',
+      name: 'Biology',
       fulfillment: {
-        courseEquivalents: {
-          EN: 358526, // CS 1110
-          AS: 358526, // CS 1110
-        },
-        minimumScore: 5,
+        minimumScore: 4,
         credits: 4,
+      },
+    },
+    {
+      name: 'Biology',
+      fulfillment: {
+        minimumScore: 5,
+        credits: 8,
       },
     },
     {
       name: 'Chemistry',
       fulfillment: {
         courseEquivalents: {
+          DEFAULT: 351265, // CHEM 2070
           EN: 359187, // CHEM 2090
-          AS: 351265, // CHEM 2070
-          AR: 351265, // CHEM 2070
         },
         minimumScore: 5,
         credits: 4,
-        majorsExcluded: ['CHEM'],
+      },
+    },
+    {
+      name: 'Computer Science A',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 358526, // CS 1110
+        },
+        minimumScore: 5,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Microeconomics',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 350025, // ECON 1110
+        },
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'Microeconomics',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 350025, // ECON 1110
+          BU: [350025, 351468], // ECON 1110, HADM 1410
+        },
+        minimumScore: 5,
+        credits: 3,
+      },
+    },
+    {
+      name: 'Macroeconomics',
+      fulfillment: {
+        courseEquivalents: {
+          // ECON 1120
+          DEFAULT: 350038,
+        },
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'English Literature and Composition',
+      fulfillment: {
+        courseEquivalents: {
+          AS: 100000, // FWS
+          EN: 100000, // FWS
+        },
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'English Language and Composition',
+      fulfillment: {
+        courseEquivalents: {
+          AS: 100000, // FWS
+          EN: 100000, // FWS
+        },
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'French Language',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 353172, // FREN 2090
+        },
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'French Literature',
+      fulfillment: {
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'Italian Language',
+      fulfillment: {
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'Italian Literature',
+      fulfillment: {
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'Mathematics BC (Non-Engineering)',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: [352116, 352120], // MATH 1110, MATH 1120
+          EN: [],
+        },
+        minimumScore: 4,
+        credits: 8,
+      },
+    },
+    {
+      name: 'Mathematics BC (Engineering)',
+      fulfillment: {
+        courseEquivalents: {
+          EN: 352255, // MATH 1910
+        },
+        minimumScore: 5,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Mathematics AB',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 352116, // MATH 1110
+        },
+        minimumScore: 4,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Physics I',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 355142, // PHYS 1101
+        },
+        minimumScore: 5,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Physics II',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 355143, // PHYS 1102
+        },
+        minimumScore: 5,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Physics C-Mechanics',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 355197, // PHYS 2207
+          EN: 355146, // PHYS 1112
+        },
+        minimumScore: 5,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Physics C-Electricity & Magnetism',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 355207, // PHYS 2213
+        },
+        minimumScore: 5,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Psychology',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: 351438, // PSYCH 1101
+        },
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'Spanish Language',
+      fulfillment: {
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'Spanish Literature',
+      fulfillment: {
+        minimumScore: 4,
+        credits: 3,
+      },
+    },
+    {
+      name: 'Statistics',
+      fulfillment: {
+        courseEquivalents: {
+          DEFAULT: [
+            350500, // AEM 2100
+            360952, // BTRY 3010
+            352353, // BTRY 6010
+            350154, // ILRST 2100
+            352353, // ILRST 6100
+            352245, // MATH 1710
+            351485, // PAM 2100
+            363824, // PAM 2101
+            351674, // PSYCH 2500
+            354665, // SOC 3010
+            350154, // STSCI 2100
+            365785, // STSCI 2150
+            360952, // STSCI 2200
+          ],
+          BU: [],
+          EN: [],
+        },
+        minimumScore: 4,
+        credits: 4,
       },
     },
   ],
@@ -129,318 +359,57 @@ export const examData: ExamData = {
         majorsExcluded: ['CHEM'],
       },
     },
+    {
+      name: 'Chemistry',
+      fulfillment: {
+        courseEquivalents: {
+          EN: 359187, // CHEM 2090
+          AS: 351265, // CHEM 2070
+          AR: 351265, // CHEM 2070
+        },
+        minimumScore: 6,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Computer Science',
+      fulfillment: {
+        courseEquivalents: {
+          // CS 1110
+          EN: 358526,
+          AS: 358526,
+        },
+        minimumScore: 6,
+        credits: 4,
+      },
+    },
+    {
+      name: 'Economics', // TODO
+      fulfillment: {
+        courseEquivalents: {},
+        minimumScore: 6,
+        credits: 4,
+      },
+    },
+    {
+      name: 'English Literature A',
+      fulfillment: {
+        courseEquivalents: {
+          // TODO
+        },
+        minimumScore: 7,
+        credits: 3,
+      },
+    },
+    {
+      name: 'English Language and Literature',
+      fulfillment: {
+        courseEquivalents: {
+          // TODO
+        },
+        minimumScore: 7,
+        credits: 3,
+      },
+    },
   ],
 };
-
-// const transferJSON: ExamData = {
-//   AP: [
-//     {
-//       subject: 'Computer Science A',
-//       credits: [
-//         {
-//           operator: 'or',
-//           collegesApplied: ['EN', 'AS'],
-//           majorExcluded: [],
-//           mininmumScore: 5,
-//           courseEquivalents: ['CS 1110'],
-//           requirementEquivalents: [],
-//           credits: 4,
-//         },
-//       ],
-//     },
-//     {
-//       subject: 'Chemistry',
-//       credits: [
-//         {
-//           operator: 'or',
-//           collegesApplied: ['EN', 'AS', 'AR'],
-//           majorExcluded: ['CHEM'],
-//           mininmumScore: 5,
-//           courseEquivalents: ['CHEM 2070', 'CHEM 2090'],
-//           requirementEquivalents: [],
-//           credits: 4,
-//         },
-//       ],
-//     },
-//     {
-//       subject: 'Microeconomics',
-//       credits: [
-//         {
-//           collegesApplied: ['EN', 'AS'],
-//           majorExcluded: [],
-//           mininmumScore: 4,
-//           operator: 'and',
-//           courseEquivalents: ['ECON 1110', 'HADM 1410'], // and?
-//           requirementEquivalents: [],
-//           credits: 4,
-//         },
-//       ],
-//     },
-//     {
-//       subject: 'Macroeconomics',
-//       credits: [
-//         {
-//           operator: 'and',
-//           collegesApplied: ['EN', 'AS'],
-//           majorExcluded: [],
-//           mininmumScore: 4,
-//           courseEquivalents: ['ECON 1120'],
-//           requirementEquivalents: [],
-//           credits: 4,
-//         },
-//       ],
-//     } /* ,
-//         {
-//             /*
-//             // TODO
-//             subject: "English Literature and Composition",
-//             credits: [4],
-//             collegesApplied: ["EN", "AS"],
-//             majorExcluded: [],
-//             mininmumScore: 5,
-//             courseEquivalents: [],
-//             requirementEquivalents: []
-//         },
-//         {
-//             // TODO
-//             subject: "English Language and Composition",
-//             credits: [4],
-//             collegesApplied: ["EN", "AS"],
-//             majorExcluded: [],
-//             mininmumScore: 5,
-//             courseEquivalents: [],
-//             requirementEquivalents: []
-//         },
-//         {
-//             subject: "French Language",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 5,
-//             courseEquivalents: [],
-//             requirementEquivalents: []
-//         },
-//         {
-//             subject: "French Literature",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [['FREN 2090']],
-//             requirementEquivalents: []
-//         },
-//         {
-//             subject: "German",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [], // TODO
-//             requirementEquivalents: []
-//         },
-//         {
-//             // TODO
-//             subject: "Greek, Ancient",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 5,
-//             courseEquivalents: [],
-//             requirementEquivalents: []
-//         },
-//         {
-//             // TODO
-//             subject: "Italian Language",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [],
-//             requirementEquivalents: []
-//         },
-//         {
-//             // TODO
-//             subject: "Italian Literature",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [],
-//             requirementEquivalents: []
-//         },
-//         {
-//             subject: "Mathematics BC",
-//             credits: [8],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [['MATH 1106', 'MATH 1110'],
-//                                 ['MATH 1210', 'MATH 1220', 'Math 1910']],
-//             requirementEquivalents: []
-//         },
-//         {
-//             subject: "Mathematics AB",
-//             credits: [4],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [['MATH 1106', 'MATH 1110']],
-//             requirementEquivalents: []
-//         },
-//         {
-//             // TODO
-//             subject: "Physics I",
-//             credits: [4],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 5,
-//             courseEquivalents: [['PHYS 1101']],
-//             requirementEquivalents: [],
-//             compositeRequirement: [
-//                 {
-//                     subject: 'Mathematics BC',
-//                     score: 5,
-//                     classEquivalent: [['PHYS 2207']]
-//                 }
-//             ]
-//         },
-//         {
-//             // TODO
-//             subject: "Physics II",
-//             credits: [4],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 5,
-//             courseEquivalents: [['PHYS 1102']],
-//             requirementEquivalents: [],
-//             compositeRequirement: [
-//                 {
-//                     subject: 'Mathematics BC',
-//                     score: 5,
-//                     classEquivalent: [['PHYS 2208']]
-//                 }
-//             ]
-//         },
-//         {
-//             // TODO
-//             subject: "Physics C-Mechanism",
-//             credits: [4],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 5,
-//             courseEquivalents: [['PHYS 1102', 'PHYS 2207']],
-//             requirementEquivalents: [],
-//         },
-//         {
-//             // TODO
-//             subject: "Physics C-Electricity/Magnetism",
-//             credits: [4],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 5,
-//             courseEquivalents: [['PHYS 2213']],
-//             requirementEquivalents: [],
-//         },
-//         {
-//             // TODO
-//             subject: "Psychology",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [['PYSCH 1101']],
-//             requirementEquivalents: [],
-//         },
-//         {
-//             // TODO
-//             subject: "Spanish Language",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [],
-//             requirementEquivalents: [],
-//         },
-//         {
-//             // TODO
-//             subject: "Spanish Literature",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [],
-//             requirementEquivalents: [],
-//         },
-//         {
-//             // TODO
-//             subject: "Statistics",
-//             credits: [3],
-//             collegesApplied: [],
-//             majorExcluded: [],
-//             mininmumScore: 4,
-//             courseEquivalents: [[ 'AEM 2100', 'BTRY 3010', 'BTRY 6010',
-//             'ILRST 2100', 'ILRST 6100', 'MATH 1710', 'PAM 2100',
-//             'PAM 2101', 'PSYCH 2500', 'SOC 3010', 'STSCI 2100', 'STSCI 2150', 'STSCI 2200']],
-//             requirementEquivalents: [],
-//         }
-//         */,
-//   ],
-
-//   IB: [
-//     {
-//       subject: 'Chemical And Physical Systems',
-//       credits: [
-//         {
-//           operator: 'or',
-//           collegesApplied: ['EN', 'AS', 'AR'],
-//           majorExcluded: ['CHEM'],
-//           mininmumScore: 5,
-//           courseEquivalents: ['CHEM 2070', 'CHEM 2090'],
-//           requirementEquivalents: [],
-//           credits: 4,
-//         },
-//       ],
-//     },
-//     /*
-//         {
-//             subject: "Chemistry",
-//             credits: [4],
-//             collegesApplied: ["EN", "AS"],
-//             majorExcluded: [],
-//             mininmumScore: 6,
-//             courseEquivalents: [['CHEM 2070', 'CHEM 2090']],
-//             requirementEquivalents: []
-//         },
-//         {
-//             subject: "Computer Science",
-//             credits: [4],
-//             collegesApplied: ["EN", "AS"],
-//             majorExcluded: [],
-//             mininmumScore: 6,
-//             courseEquivalents: [['CS 1110']],
-//             requirementEquivalents: []
-//         },
-//         {
-//             subject: "Economics",
-//             credits: [6],
-//             collegesApplied: ["EN", "AS"],
-//             majorExcluded: [],
-//             mininmumScore: 6,
-//             courseEquivalents: [['CS 1110']],
-//             requirementEquivalents: []
-//             // varies
-
-//         },
-//         {
-//             subject: "English",
-//             credits: [6],
-//             collegesApplied: ["EN", "AS"],
-//             majorExcluded: [],
-//             mininmumScore: 6,
-//             courseEquivalents: [['CS 1110']],
-//             requirementEquivalents: []
-//             // varies
-//         },
-//         */
-//   ],
-// };
