a483dac39437214d08a543ea6c94f05bedbe634b
Sam <sam@developersam.com>
2021-03-12 21:44:32 -0500

    Remove hardcoded numbers

--- src/assets/scss/_variables.scss.d.ts
+++ src/assets/scss/_variables.scss.d.ts
@@ -6,3 +6,8 @@ export const secondaryGray: string;
 export const lightPlaceholderGray: string;
 export const darkPlaceholderGray: string;
 export const emGreen: string;
+export const smallBreakpoint: string;
+export const smallMediumBreakpoint: string;
+export const mediumBreakpoint: string;
+export const largeBreakpoint: string;
+export const veryLargeBreakpoint: string;
--- src/containers/Dashboard.vue
+++ src/containers/Dashboard.vue
@@ -76,6 +76,31 @@ import surfing from '@/assets/images/surfing.svg';
 
 import store, { initializeFirestoreListeners } from '@/store';
 import { immutableBottomBarState } from '@/components/BottomBar/BottomBarState';
+import {
+  smallBreakpoint,
+  mediumBreakpoint,
+  veryLargeBreakpoint,
+} from '@/assets/scss/_variables.scss';
+
+const smallBreakpointPixels = parseInt(
+  smallBreakpoint.substring(0, smallBreakpoint.length - 2),
+  10
+);
+const mediumBreakpointPixels = parseInt(
+  mediumBreakpoint.substring(0, mediumBreakpoint.length - 2),
+  10
+);
+const veryLargeBreakpointPixels = parseInt(
+  veryLargeBreakpoint.substring(0, veryLargeBreakpoint.length - 2),
+  10
+);
+
+const getMaxButtonBarTabs = () => {
+  if (window.innerWidth <= smallBreakpointPixels) {
+    return window.innerWidth <= veryLargeBreakpointPixels ? 1 : 2;
+  }
+  return 4;
+};
 
 const tour = introJs();
 tour.setOption('exitOnEsc', 'false');
@@ -103,9 +128,9 @@ export default Vue.extend({
       isOnboarding: false,
       isEditingProfile: false,
       isOpeningRequirements: false,
-      isTablet: window.innerWidth <= 878,
-      isMobile: window.innerWidth <= 440,
-      maxBottomBarTabs: window.innerWidth <= 1347 ? 2 : 4,
+      isTablet: window.innerWidth <= mediumBreakpointPixels,
+      isMobile: window.innerWidth <= smallBreakpointPixels,
+      maxBottomBarTabs: getMaxButtonBarTabs(),
       welcomeHidden: false,
       startTour: false,
       showTourEndWindow: false,
@@ -149,13 +174,9 @@ export default Vue.extend({
   },
   methods: {
     resizeEventHandler() {
-      this.isMobile = window.innerWidth <= 440;
-      this.isTablet = window.innerWidth <= 878;
-      if (window.innerWidth <= 1347) {
-        this.maxBottomBarTabs = window.innerWidth <= 480 ? 1 : 2;
-      } else {
-        this.maxBottomBarTabs = 4;
-      }
+      this.isMobile = window.innerWidth <= smallBreakpointPixels;
+      this.isTablet = window.innerWidth <= mediumBreakpointPixels;
+      this.maxBottomBarTabs = getMaxButtonBarTabs();
       this.updateSemesterView();
     },
     toggleRequirementsBar() {
