3742aa7f17fc96c4b3755b24af682bbb6d26572b
einc <ein.chang@gmail.com>
2021-03-22 21:09:09 -0400

    prettier

--- src/requirements/data/colleges/bu.ts
+++ src/requirements/data/colleges/bu.ts
@@ -1,5 +1,5 @@
 import { CollegeOrMajorRequirement } from '../../types';
 
 const businessRequirements: readonly CollegeOrMajorRequirement[] = [];
-// offers two undergraduate majors: AEM and Hotel Admin 
+// offers two undergraduate majors: AEM and Hotel Admin
 export default businessRequirements;
