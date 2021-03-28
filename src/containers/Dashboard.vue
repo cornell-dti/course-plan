94f1170ab0cd4a2a457d17612e17ef8a2423f7db
Jason <mickeu420@gmail.com>
2021-03-28 18:16:05 -0400

    restore emoji text class

--- src/containers/Dashboard.vue
+++ src/containers/Dashboard.vue
@@ -246,6 +246,9 @@ export default Vue.extend({
     background-color: rgb(0, 0, 0); /* Fallback color */
     background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
   }
+  .emoji-text {
+    height: 14px;
+  }
 }
 
 .semester {
