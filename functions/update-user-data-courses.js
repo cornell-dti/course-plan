f811dc513ac6f7a1438ce84d5c910686f763c479
Sam Zhou <sam@developersam.com>
2021-03-28 18:20:44 -0400

    Add dry-run option for data migration script (#393)

--- functions/update-user-data-courses.js
+++ functions/update-user-data-courses.js
@@ -7,34 +7,44 @@ admin.initializeApp({
   databaseURL: 'https://cornelldti-courseplan-dev.firebaseio.com',
 });
 
-const filteredCoursesPaths = fs.readdirSync('./filtered_courses/');
-
-const filteredAllCourses = filteredCoursesPaths
-  .map(path => require('./filtered_courses/' + path))
-  .reduce((accum, currentValue) => Object.assign(accum, currentValue));
+const filteredAllCourses = Object.values(
+  JSON.parse(fs.readFileSync('../src/assets/courses/full-courses.json').toString())
+).flat();
 
 const db = admin.firestore();
 const userDataCollection = db.collection('userData');
 
-userDataCollection.get().then(userData => {
-  Promise.all(
-    userData.docs.map(userDataDocument => {
-      const data = userDataDocument.data();
-      return userDataDocument.ref.set({
-        ...data,
-        semesters: data.semesters.map(semester => ({
-          ...semester,
-          courses: semester.courses.map(course => {
-            const { code, lastRoster } = course;
-            const [subject, number] = code.split(' ');
-            const crseId = filteredAllCourses[lastRoster].find(
-              it => it.subject === subject && it.catalogNbr === number
-            ).crseId;
-            if (crseId == null) throw new Error(`${lastRoster}, ${subject}, ${number}, ${crseId}`);
-            return { ...course, crseId };
-          }),
-        })),
-      });
-    })
-  );
+const transformData = data => ({
+  ...data,
+  semesters: data.semesters.map(semester => ({
+    ...semester,
+    courses: semester.courses.map(course => {
+      const { code } = course;
+      const [subject, number] = code.split(' ');
+      const fullCourse = filteredAllCourses.find(
+        it => it.subject === subject && it.catalogNbr === number
+      );
+      const crseId = fullCourse && fullCourse.crseId;
+      if (crseId == null) throw new Error(`user=${data.id}, subject=${subject}, number=${number}`);
+      return { ...course, crseId };
+    }),
+  })),
 });
+
+if (process.argv[2] === '--dry-run') {
+  userDataCollection.get().then(userData => {
+    const oldUserData = userData.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    const newUserData = oldUserData.map(transformData);
+    fs.writeFileSync('old.json', JSON.stringify(oldUserData, undefined, 2));
+    fs.writeFileSync('new.json', JSON.stringify(newUserData, undefined, 2));
+  });
+  return;
+} else {
+  userDataCollection.get().then(userData => {
+    Promise.all(
+      userData.docs.map(userDataDocument =>
+        userDataDocument.ref.set(transformData(userDataDocument.data()))
+      )
+    );
+  });
+}
