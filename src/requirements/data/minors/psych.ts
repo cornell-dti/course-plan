43814408b2d0ce1d5e336aae9996e6bc71cd57c3
einc <ein.chang@gmail.com>
2021-04-04 09:54:41 -0400

    add period

--- src/requirements/data/minors/psych.ts
+++ src/requirements/data/minors/psych.ts
@@ -5,7 +5,7 @@ const psychMinorRequirements: readonly CollegeOrMajorRequirement[] = [
   {
     name: '18 Credits in Psychology',
     description:
-      'A total of 18 credits in psychology. First-year seminars or AP courses cannot be counted towards the credit requirements' +
+      'A total of 18 credits in psychology. First-year seminars or AP courses cannot be counted towards the credit requirements. ' +
       'It is strongly recommended that students take at least one course from each of the following three areas of psychology: PCD, BEN, S&P.',
     source: 'https://psychology.cornell.edu/minor',
     checker: includesWithSingleRequirement(
