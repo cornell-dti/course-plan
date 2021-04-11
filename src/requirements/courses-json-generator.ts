6e05670dc272c179ae54baf51c30f84cbde4a29c
Sam Zhou <sam@developersam.com>
2021-04-11 17:40:31 -0400

    [easy] Remove simplified course json generator (#456)

--- src/requirements/courses-json-generator.ts
+++ src/requirements/courses-json-generator.ts
@@ -2,24 +2,6 @@ import { writeFileSync } from 'fs';
 import { courseCodeToDataAndOfferedMap } from './filtered-course-data';
 import { Course } from './types';
 
-export type CourseJson = {
-  [courseCode: string]: {
-    i: number; // course id
-    t: string; // title
-    r: string; // roster (aka semester)
-  };
-};
-
-const courseJson: CourseJson = Object.fromEntries(
-  Array.from(courseCodeToDataAndOfferedMap.values()).map(
-    ([semester, course]) =>
-      [
-        `${course.subject} ${course.catalogNbr}`,
-        { i: course.crseId, t: course.titleLong, r: semester },
-      ] as const
-  )
-);
-
 const fullCourseJson = (() => {
   const json: Record<number, Course & { roster: string }[]> = {};
 
@@ -32,5 +14,4 @@ const fullCourseJson = (() => {
   return json;
 })();
 
-writeFileSync('src/assets/courses/courses.json', JSON.stringify(courseJson));
 writeFileSync('src/assets/courses/full-courses.json', JSON.stringify(fullCourseJson));
