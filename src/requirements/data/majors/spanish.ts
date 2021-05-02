a4ddd25d2daddf22683e11943199280b7072a7f3
Vaishnavi Gupta <vg222@cornell.edu>
2021-05-02 10:45:16 -0400

    updated spanish major to use subreqs

--- src/requirements/data/majors/spanish.ts
+++ src/requirements/data/majors/spanish.ts
@@ -9,54 +9,18 @@ import {
 
 const spanishRequirements: readonly CollegeOrMajorRequirement[] = [
   {
-    name: 'Requirement 1',
-    description: 'SPAN 2140 or SPAN 2235',
+    name: 'Core Courses',
+    description: 'Students are required to complete 4 core courses.',
     source: 'https://romancestudies.cornell.edu/spanish-undergraduate#major-requirements',
-    checker: includesWithSingleRequirement('SPAN 2140', 'SPAN 2235'),
+    checker: includesWithSubRequirements(
+      ['SPAN 2140', 'SPAN 2235'],
+      ['SPAN 2150', 'SPAN 2205'],
+      ['SPAN 2170'],
+      ['SPAN 2180']
+    ),
     fulfilledBy: 'courses',
-    perSlotMinCount: [1],
-    slotNames: ['Course'],
-  },
-  {
-    name: 'Requirement 2',
-    description: 'SPAN 2150 or SPAN 2205',
-    source: 'https://romancestudies.cornell.edu/spanish-undergraduate#major-requirements',
-    checker: includesWithSingleRequirement('SPAN 2150', 'SPAN 2205'),
-    fulfilledBy: 'courses',
-    perSlotMinCount: [1],
-    slotNames: ['Course'],
-  },
-  {
-    name: 'Requirement 3',
-    description: 'SPAN 2170',
-    source: 'https://romancestudies.cornell.edu/spanish-undergraduate#major-requirements',
-    checker: includesWithSingleRequirement('SPAN 2170'),
-    fulfilledBy: 'courses',
-    perSlotMinCount: [1],
-    slotNames: ['Course'],
-  },
-  {
-    name: 'Requirement 4',
-    description: 'SPAN 2180',
-    source: 'https://romancestudies.cornell.edu/spanish-undergraduate#major-requirements',
-    checker: includesWithSingleRequirement('SPAN 2180'),
-    fulfilledBy: 'courses',
-    perSlotMinCount: [1],
-    slotNames: ['Course'],
-  },
-  {
-    name: 'Electives',
-    description: '15 credits of electives of courses focusing on the Hispanic world. SPAN 2090, SPAN 2070 and SPAN 2095 are excluded. Only courses beginning at SPAN 2130 count.',
-    source: 'https://romancestudies.cornell.edu/spanish-undergraduate#major-requirements',
-    checker: [
-      (course: Course): boolean => {
-        return (
-          ((ifCodeMatch(course.subject, 'SPAN') || ifCodeMatch(course.subject, 'PORT')) && !(ifCodeMatch(course.catalogNbr, '1***') || ifCodeMatch(course.catalogNbr, '20**')))
-        );
-      },
-    ],
-    fulfilledBy: 'credits',
-    perSlotMinCount: [15],
+    perSlotMinCount: [1, 1, 1, 1],
+    slotNames: ['SPAN 2140 or SPAN 2235', 'SPAN 2150 or SPAN 2205', 'SPAN 2170', 'SPAN 2180'],
   },
   // TODO: see if there is a way to identify senior seminar courses.
   {
