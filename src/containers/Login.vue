b7ab26704af9c15efede5754186e5b8c5e7c3494
Sam Zhou <sam@developersam.com>
2021-09-29 21:41:37 -0400

    Setup typed firebase admin database collections (#531)

--- src/containers/Login.vue
+++ src/containers/Login.vue
@@ -150,7 +150,7 @@ import firebase from 'firebase/app';
 import CustomFooter from '@/components/Footer.vue';
 
 import { GTagLoginEvent } from '@/gtag';
-import * as fb from '@/firebaseConfig';
+import * as fb from '@/firebase-frontend-config';
 import store from '@/store';
 import { checkNotNull } from '@/utilities';
 
