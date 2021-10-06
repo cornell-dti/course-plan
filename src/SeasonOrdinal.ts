d79bee9a850d2668330e1668986f2d1c527eba7c
Zak Kent <zak33@cornell.edu>
2021-10-06 00:46:03 -0400

    Simplfied SeasonOrdinal export

--- src/SeasonOrdinal.ts
+++ src/SeasonOrdinal.ts
@@ -1,9 +1,7 @@
 /** enum to define seasons as integers in season order */
-const SeasonOrdinal = {
+export default {
   Winter: 0,
   Spring: 1,
   Summer: 2,
   Fall: 3,
 } as const;
-
-export default SeasonOrdinal;
