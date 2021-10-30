bee0481850eef4aa1aa07a2d73038565b29885a0
Benjamin Shen <bfs45@cornell.edu>
2021-10-29 22:06:19 -0400

    comment unused import

--- src/requirements/data/index.ts
+++ src/requirements/data/index.ts
@@ -64,7 +64,7 @@ import psychMinorRequirements from './minors/psych';
 import roboticsMinorRequirements from './minors/robotics';
 import spanishMinorRequirements from './minors/spanish';
 
-import mengCSRequirements from './grad/meng-cs';
+// import mengCSRequirements from './grad/meng-cs';
 import mpaRequirements from './grad/mpa';
 
 const json: RequirementsJson = {
