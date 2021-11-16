975785083b9ae6489ea945ae623c108dad9e3fa6
Benjamin Shen <bfs45@cornell.edu>
2021-11-16 15:48:03 -0500

    Update exam data schema, add course ID to each fulfillment option (#584)
    * add courseid to exam data
    
    * update schema
    
    * fix tests
    
    * update schema again, update requirements json, userExamsToExamCourses
    
    * Fix tests
    
    * Fix lint issues
    
    * Implement userDataToExamCourses
    
    * Fix lint errors
    
    * revert deprecated fws, add exam fulfillment comment

--- src/requirements/data/exams/ExamCredit.ts
+++ src/requirements/data/exams/ExamCredit.ts
@@ -1,35 +1,29 @@
 import { FWS_COURSE_ID } from '../constants';
 
-export type ExamRequirements = {
-  readonly name: string;
-  readonly fulfillment: {
-    readonly courseEquivalents?: Record<string, number[]>;
-    readonly minimumScore: number;
-    readonly credits: number;
-    readonly majorsExcluded?: string[];
-  };
+/**
+ * Describes how exams can be converted to a representation understood by CoursePlan.
+ * If the user takes an exam, our algorithm determines the best fulfillment option for the user's score.
+ * There can be multiple exam fulfillments (for different minimum scores) associated with a single exam.
+ */
+export type ExamFulfillment = {
+  readonly courseId: number;
+  readonly courseEquivalents?: Record<string, number[]>;
+  readonly minimumScore: number;
+  readonly credits: number;
+  readonly majorsExcluded?: string[];
 };
-export type ExamData = Record<'AP' | 'IB', ExamRequirements[]>;
+export type ExamFulfillments = Record<string, ExamFulfillment[]>;
+export type ExamData = Record<'AP' | 'IB', ExamFulfillments>;
 
 const examData: ExamData = {
-  AP: [
-    {
-      name: 'Biology',
-      fulfillment: {
-        minimumScore: 4,
-        credits: 4,
-      },
-    },
-    {
-      name: 'Biology',
-      fulfillment: {
-        minimumScore: 5,
-        credits: 8,
-      },
-    },
-    {
-      name: 'Chemistry',
-      fulfillment: {
+  AP: {
+    Biology: [
+      { courseId: 100, minimumScore: 4, credits: 4 },
+      { courseId: 101, minimumScore: 5, credits: 8 },
+    ],
+    Chemistry: [
+      {
+        courseId: 102,
         courseEquivalents: {
           DEFAULT: [351265], // CHEM 2070
           EN: [359187], // CHEM 2090
@@ -37,30 +31,28 @@ const examData: ExamData = {
         minimumScore: 5,
         credits: 4,
       },
-    },
-    {
-      name: 'Computer Science A',
-      fulfillment: {
+    ],
+    'Computer Science A': [
+      {
+        courseId: 103,
         courseEquivalents: {
           DEFAULT: [358526], // CS 1110
         },
         minimumScore: 5,
         credits: 4,
       },
-    },
-    {
-      name: 'Microeconomics',
-      fulfillment: {
+    ],
+    Microeconomics: [
+      {
+        courseId: 104,
         courseEquivalents: {
           DEFAULT: [350025], // ECON 1110
         },
         minimumScore: 4,
         credits: 3,
       },
-    },
-    {
-      name: 'Microeconomics',
-      fulfillment: {
+      {
+        courseId: 105,
         courseEquivalents: {
           DEFAULT: [350025], // ECON 1110
           BU: [350025, 351468], // ECON 1110, HADM 1410
@@ -68,71 +60,53 @@ const examData: ExamData = {
         minimumScore: 5,
         credits: 3,
       },
-    },
-    {
-      name: 'Macroeconomics',
-      fulfillment: {
+    ],
+    Macroeconomics: [
+      {
+        courseId: 106,
         courseEquivalents: {
           DEFAULT: [350038], // ECON 1120
         },
         minimumScore: 4,
         credits: 3,
       },
-    },
-    {
-      name: 'English Literature and Composition',
-      fulfillment: {
+    ],
+    'English Literature and Composition': [
+      {
+        courseId: 107,
         courseEquivalents: {
           DEFAULT: [FWS_COURSE_ID], // FWS
         },
         minimumScore: 4,
         credits: 3,
       },
-    },
-    {
-      name: 'English Language and Composition',
-      fulfillment: {
+    ],
+    'English Language and Composition': [
+      {
+        courseId: 108,
         courseEquivalents: {
           DEFAULT: [FWS_COURSE_ID], // FWS
         },
         minimumScore: 4,
         credits: 3,
       },
-    },
-    {
-      name: 'French Language',
-      fulfillment: {
+    ],
+    'French Language': [
+      {
+        courseId: 109,
         courseEquivalents: {
           DEFAULT: [353172], // FREN 2090
         },
         minimumScore: 4,
         credits: 3,
       },
-    },
-    {
-      name: 'French Literature',
-      fulfillment: {
-        minimumScore: 4,
-        credits: 3,
-      },
-    },
-    {
-      name: 'Italian Language',
-      fulfillment: {
-        minimumScore: 4,
-        credits: 3,
-      },
-    },
-    {
-      name: 'Italian Literature',
-      fulfillment: {
-        minimumScore: 4,
-        credits: 3,
-      },
-    },
-    {
-      name: 'Mathematics BC (Non-Engineering)',
-      fulfillment: {
+    ],
+    'French Literature': [{ courseId: 110, minimumScore: 4, credits: 3 }],
+    'Italian Language': [{ courseId: 111, minimumScore: 4, credits: 3 }],
+    'Italian Literature': [{ courseId: 112, minimumScore: 4, credits: 3 }],
+    'Mathematics BC (Non-Engineering)': [
+      {
+        courseId: 113,
         courseEquivalents: {
           DEFAULT: [352116, 352120], // MATH 1110, MATH 1120
           EN: [],
@@ -140,10 +114,10 @@ const examData: ExamData = {
         minimumScore: 4,
         credits: 8,
       },
-    },
-    {
-      name: 'Mathematics BC (Engineering)',
-      fulfillment: {
+    ],
+    'Mathematics BC (Engineering)': [
+      {
+        courseId: 114,
         courseEquivalents: {
           DEFAULT: [],
           EN: [352255], // MATH 1910
@@ -151,40 +125,40 @@ const examData: ExamData = {
         minimumScore: 5,
         credits: 4,
       },
-    },
-    {
-      name: 'Mathematics AB',
-      fulfillment: {
+    ],
+    'Mathematics AB': [
+      {
+        courseId: 115,
         courseEquivalents: {
           DEFAULT: [352116], // MATH 1110
         },
         minimumScore: 4,
         credits: 4,
       },
-    },
-    {
-      name: 'Physics I',
-      fulfillment: {
+    ],
+    'Physics I': [
+      {
+        courseId: 116,
         courseEquivalents: {
           DEFAULT: [355142], // PHYS 1101
         },
         minimumScore: 5,
         credits: 4,
       },
-    },
-    {
-      name: 'Physics II',
-      fulfillment: {
+    ],
+    'Physics II': [
+      {
+        courseId: 117,
         courseEquivalents: {
           DEFAULT: [355143], // PHYS 1102
         },
         minimumScore: 5,
         credits: 4,
       },
-    },
-    {
-      name: 'Physics C-Mechanics',
-      fulfillment: {
+    ],
+    'Physics C-Mechanics': [
+      {
+        courseId: 118,
         courseEquivalents: {
           DEFAULT: [355197], // PHYS 2207
           EN: [355146], // PHYS 1112
@@ -192,44 +166,44 @@ const examData: ExamData = {
         minimumScore: 5,
         credits: 4,
       },
-    },
-    {
-      name: 'Physics C-Electricity & Magnetism',
-      fulfillment: {
+    ],
+    'Physics C-Electricity & Magnetism': [
+      {
+        courseId: 119,
         courseEquivalents: {
           DEFAULT: [355207], // PHYS 2213
         },
         minimumScore: 5,
         credits: 4,
       },
-    },
-    {
-      name: 'Psychology',
-      fulfillment: {
+    ],
+    Psychology: [
+      {
+        courseId: 120,
         courseEquivalents: {
           DEFAULT: [351438], // PSYCH 1101
         },
         minimumScore: 4,
         credits: 3,
       },
-    },
-    {
-      name: 'Spanish Language',
-      fulfillment: {
+    ],
+    'Spanish Language': [
+      {
+        courseId: 120,
         minimumScore: 4,
         credits: 3,
       },
-    },
-    {
-      name: 'Spanish Literature',
-      fulfillment: {
+    ],
+    'Spanish Literature': [
+      {
+        courseId: 120,
         minimumScore: 4,
         credits: 3,
       },
-    },
-    {
-      name: 'Statistics',
-      fulfillment: {
+    ],
+    Statistics: [
+      {
+        courseId: 121,
         courseEquivalents: {
           DEFAULT: [
             350500, // AEM 2100
@@ -254,10 +228,8 @@ const examData: ExamData = {
         credits: 4,
         majorsExcluded: ['Biological Sciences'],
       },
-    },
-    {
-      name: 'Statistics',
-      fulfillment: {
+      {
+        courseId: 122,
         courseEquivalents: {
           DEFAULT: [
             350500, // AEM 2100
@@ -280,22 +252,22 @@ const examData: ExamData = {
         minimumScore: 5,
         credits: 4,
       },
-    },
-  ],
-  IB: [
-    {
-      name: 'Chemical and Physical Systems',
-      fulfillment: {
+    ],
+  },
+  IB: {
+    'Chemical and Physical Systems': [
+      {
+        courseId: 200,
         courseEquivalents: {
           DEFAULT: [355142, 355143], // PHYS 1101, PHYS 1102
         },
         minimumScore: 6,
         credits: 8,
       },
-    },
-    {
-      name: 'Chemistry',
-      fulfillment: {
+    ],
+    Chemistry: [
+      {
+        courseId: 202,
         courseEquivalents: {
           DEFAULT: [351265], // CHEM 2070
           EN: [359187], // CHEM 2090
@@ -303,70 +275,70 @@ const examData: ExamData = {
         minimumScore: 6,
         credits: 4,
       },
-    },
-    {
-      name: 'Computer Science',
-      fulfillment: {
+    ],
+    'Computer Science': [
+      {
+        courseId: 203,
         courseEquivalents: {
           DEFAULT: [358526], // CS 1110
         },
         minimumScore: 6,
         credits: 4,
       },
-    },
-    {
-      name: 'Economics',
-      fulfillment: {
+    ],
+    Economics: [
+      {
+        courseId: 204,
         courseEquivalents: {
           DEFAULT: [350025, 350038], // ECON 1110, ECON 1120
         },
         minimumScore: 6,
         credits: 6,
       },
-    },
-    {
-      name: 'English Literature A',
-      fulfillment: {
+    ],
+    'English Literature A': [
+      {
+        courseId: 205,
         courseEquivalents: {
           DEFAULT: [FWS_COURSE_ID], // FWS
         },
         minimumScore: 7,
         credits: 3,
       },
-    },
-    {
-      name: 'English Language and Literature',
-      fulfillment: {
+    ],
+    'English Language and Literature': [
+      {
+        courseId: 206,
         courseEquivalents: {
           DEFAULT: [FWS_COURSE_ID], // FWS
         },
         minimumScore: 7,
         credits: 3,
       },
-    },
-    {
-      name: 'Mathematics',
-      fulfillment: {
+    ],
+    Mathematics: [
+      {
+        courseId: 207,
         courseEquivalents: {
           DEFAULT: [352111, 352116], // MATH 1106, MATH 1110
         },
         minimumScore: 6,
         credits: 4,
       },
-    },
-    {
-      name: 'Physical Science',
-      fulfillment: {
+    ],
+    'Physical Science': [
+      {
+        courseId: 208,
         courseEquivalents: {
           DEFAULT: [351265, 355142], // CHEM 2070, PHYS 1101
         },
         minimumScore: 6,
         credits: 8,
       },
-    },
-    {
-      name: 'Physics',
-      fulfillment: {
+    ],
+    Physics: [
+      {
+        courseId: 209,
         courseEquivalents: {
           DEFAULT: [355142, 355197], // PHYS 1101, PHYS 2207
           EN: [355146], // PHYS 1112
@@ -374,8 +346,8 @@ const examData: ExamData = {
         minimumScore: 6,
         credits: 4,
       },
-    },
-  ],
+    ],
+  },
 };
 
 export default examData;
--- src/requirements/requirement-exam-utils.ts
+++ src/requirements/requirement-exam-utils.ts
@@ -1,18 +1,28 @@
-import examData, { ExamRequirements } from './data/exams/ExamCredit';
+import examData, { ExamFulfillment, ExamFulfillments } from './data/exams/ExamCredit';
 import { NO_EQUIVALENT_COURSES_COURSE_ID, CREDITS_COURSE_ID } from './data/constants';
 
-type ExamTaken = {
+/** @deprecated old infra */
+type ExamTakenOld = {
   readonly subject: string;
   readonly score: number;
 };
-export type ExamsTaken = Record<'AP' | 'IB', ExamTaken[]>;
+/** @deprecated old infra */
+export type ExamsTakenOld = Record<'AP' | 'IB', ExamTakenOld[]>;
 
-type ExamSubjects = Record<'AP' | 'IB', string[]>;
+type ExamType = keyof typeof examData;
+type ExamTaken = {
+  readonly examType: ExamType;
+  readonly subject: string;
+  readonly score: number;
+};
+export type ExamsTaken = ExamTaken[];
+type ExamSubjects = Record<ExamType, string[]>;
 
+/** @deprecated old infra */
 function userDataToCourses(
   college: string | undefined,
   major: string | undefined,
-  userData: ExamsTaken,
+  userData: ExamsTakenOld,
   examType: 'AP' | 'IB'
 ): CourseTaken[] {
   // If there is no college, that means that the user only has a grad program, so they cannot get any course credit.
@@ -23,11 +33,12 @@ function userDataToCourses(
   const courses: CourseTaken[] = [];
   userExams.forEach(userExam => {
     // match exam to user-taken exam
-    const exam = exams.reduce((prev: ExamRequirements | undefined, curr: ExamRequirements) => {
+    const exam = exams[userExam.subject];
+    const fulfillment = exam.reduce((prev: ExamFulfillment | undefined, curr: ExamFulfillment) => {
       // check if exam name matches and score is high enough
-      if (curr.name.includes(userExam.subject) && userExam.score >= curr.fulfillment.minimumScore) {
+      if (userExam.score >= curr.minimumScore) {
         // update exam variable if this exam has a higher minimum score
-        if (!prev || prev.fulfillment.minimumScore < curr.fulfillment.minimumScore) {
+        if (!prev || prev.minimumScore < curr.minimumScore) {
           return curr;
         }
       }
@@ -37,14 +48,13 @@ function userDataToCourses(
     // multiple equivalent courses for the same exam can share a unique id, i.e., the unique id represents the exam id
     let courseEquivalentsExist = false;
     const name = `${examType} ${userExam.subject}`;
-    if (exam) {
+    if (fulfillment) {
       const courseEquivalents =
-        (exam.fulfillment.courseEquivalents &&
-          (exam.fulfillment.courseEquivalents[college] ||
-            exam.fulfillment.courseEquivalents.DEFAULT)) ||
+        (fulfillment.courseEquivalents &&
+          (fulfillment.courseEquivalents[college] || fulfillment.courseEquivalents.DEFAULT)) ||
         [];
       const excludedMajor =
-        major && exam.fulfillment.majorsExcluded && exam.fulfillment.majorsExcluded.includes(major);
+        major && fulfillment.majorsExcluded && fulfillment.majorsExcluded.includes(major);
       if (!excludedMajor) {
         // AP/IB credit can be potentially applied towards the user's requirements
         courseEquivalentsExist = true;
@@ -54,7 +64,7 @@ function userDataToCourses(
             courseId,
             uniqueId: name,
             code: name,
-            credits: exam.fulfillment.credits,
+            credits: fulfillment.credits,
           });
         } else {
           // separate credits from equivalent course
@@ -62,7 +72,7 @@ function userDataToCourses(
             courseId: CREDITS_COURSE_ID,
             uniqueId: name,
             code: name,
-            credits: exam.fulfillment.credits,
+            credits: fulfillment.credits,
           });
           courseEquivalents.forEach(courseId => {
             courses.push({
@@ -87,24 +97,26 @@ function userDataToCourses(
   return courses;
 }
 
+/** @deprecated old infra */
 export function getCourseEquivalentsFromOneMajor(
   college: string | undefined,
   major: string | undefined,
-  userData: ExamsTaken
+  userData: ExamsTakenOld
 ): readonly CourseTaken[] {
   const APCourseEquivalents = userDataToCourses(college, major, userData, 'AP');
   const IBCourseEquivalents = userDataToCourses(college, major, userData, 'IB');
   return APCourseEquivalents.concat(IBCourseEquivalents);
 }
 
-export default function getCourseEquivalentsFromUserExams(
+/** @deprecated old infra */
+export default function getCourseEquivalentsFromOneUserExam(
   user: AppOnboardingData
 ): readonly CourseTaken[] {
   const examCourseCodeSet = new Set<string>();
   const { college, major: majors } = user;
-  const userExamData: ExamsTaken = { AP: [], IB: [] };
+  const userExamData: ExamsTakenOld = { AP: [], IB: [] };
   user.exam.forEach((exam: FirestoreAPIBExam) => {
-    const examTaken: ExamTaken = { subject: exam.subject, score: exam.score };
+    const examTaken: ExamTakenOld = { subject: exam.subject, score: exam.score };
     userExamData[exam.type].push(examTaken);
   });
   if (majors.length === 0) {
@@ -123,12 +135,64 @@ export default function getCourseEquivalentsFromUserExams(
   ].flat();
 }
 
-function toSubjects(data: ExamRequirements[]) {
-  const exams = data.map(({ name }) => name);
-  const subjects = [...new Set(exams)];
+const getExamFulfillment = (userExam: ExamTaken): ExamFulfillment | undefined => {
+  const exam = examData[userExam.examType][userExam.subject];
+  const fulfillment = exam.reduce((prev: ExamFulfillment | undefined, curr: ExamFulfillment) => {
+    // check if exam name matches and score is high enough
+    if (userExam.score >= curr.minimumScore) {
+      // update exam variable if this exam has a higher minimum score
+      if (!prev || prev.minimumScore < curr.minimumScore) {
+        return curr;
+      }
+    }
+    return prev;
+  }, undefined);
+  return fulfillment;
+};
+
+export const examsTakenToExamCourses = (exams: ExamsTaken): CourseTaken[] => {
+  const examCourses: CourseTaken[] = [];
+  exams.forEach(exam => {
+    // match exam to fulfillment
+    const fulfillment = getExamFulfillment(exam);
+
+    // generate exam course
+    const examName = `${exam.examType} ${exam.subject}`;
+    if (fulfillment) {
+      const { courseId, credits } = fulfillment;
+      examCourses.push({
+        courseId,
+        uniqueId: examName,
+        code: examName,
+        credits,
+      });
+    } else {
+      examCourses.push({
+        courseId: NO_EQUIVALENT_COURSES_COURSE_ID,
+        uniqueId: examName,
+        code: examName,
+        credits: 0,
+      });
+    }
+  });
+  return examCourses;
+};
+
+// TODO @bshen make this default export
+export const userDataToExamCourses = (user: AppOnboardingData): CourseTaken[] => {
+  const examsTaken = user.exam.map(({ type: examType, subject, score }) => ({
+    examType,
+    subject,
+    score,
+  }));
+  return examsTakenToExamCourses(examsTaken);
+};
+
+const toSubjects = (data: ExamFulfillments) => {
+  const subjects = [...new Set(Object.keys(data))];
   subjects.sort();
   return subjects;
-}
+};
 
 export const examSubjects: ExamSubjects = {
   AP: toSubjects(examData.AP),
@@ -136,12 +200,10 @@ export const examSubjects: ExamSubjects = {
 };
 
 export const getExamCredit = (examTaken: FirestoreAPIBExam): number => {
-  const examsWithSameName: ExamRequirements[] = examData[examTaken.type].filter(
-    it => it.name === examTaken.subject
-  );
-  const mostPossibleCredit = examsWithSameName.reduce((credit, exam) => {
-    if (examTaken.score >= exam.fulfillment.minimumScore) {
-      return Math.max(credit, exam.fulfillment.credits);
+  const exam = examData[examTaken.type][examTaken.subject];
+  const mostPossibleCredit = exam.reduce((credit, fulfillment) => {
+    if (examTaken.score >= fulfillment.minimumScore) {
+      return Math.max(credit, fulfillment.credits);
     }
     return credit;
   }, 0);
