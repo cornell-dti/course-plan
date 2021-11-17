977ea6f2d8848479fda9ba62c9a5cd025a6860ac
Benjamin Shen <bfs45@cornell.edu>
2021-11-16 21:06:18 -0500

    semester season migration (#593)

---
+++ src/requirements/admin/semester-season-migration.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+
+import { usernameCollection, semestersCollection } from '../../firebase-admin-config';
+
+/**
+ * Perform migration of semester type to semester season
+ */
+async function runOnUser(userEmail: string, runOnDB: boolean) {
+  const semesters = await semestersCollection
+    .doc(userEmail)
+    .get()
+    .then(it => {
+      const data = it.data();
+      return (data && data.semesters) || [];
+    });
+
+  console.group(`Running on ${userEmail}...`);
+  const newSemesters = semesters.map(semester =>
+    // each semester should have type but not necessarily season
+    ({ ...semester, season: semester.season || semester.type })
+  );
+  console.log(semesters, '=>', newSemesters);
+  console.groupEnd();
+
+  if (runOnDB) {
+    await semestersCollection.doc(userEmail).update({ semesters: newSemesters });
+  }
+}
+
+async function main() {
+  let userEmail = process.argv[2];
+  const runOnDB = process.argv.includes('--run-on-db');
+  if (userEmail != null && userEmail !== '--run-on-db') {
+    await runOnUser(userEmail, runOnDB);
+    return;
+  }
+  const collection = await usernameCollection.get();
+  const userEmails = collection.docs.map(it => it.id);
+  for (userEmail of userEmails) {
+    console.group(`Running on ${userEmail}...`);
+    // Intentionally await in a loop to have no interleaved console logs.
+    // eslint-disable-next-line no-await-in-loop
+    await runOnUser(userEmail, runOnDB);
+    console.groupEnd();
+  }
+}
+
+main();
