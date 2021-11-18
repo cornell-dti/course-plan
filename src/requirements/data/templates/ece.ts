8c19025ef7464748b0b7567ae613fc705981ac93
Aryan Yadav <yadavaryan424@gmail.com>
2021-11-18 13:38:26 -0500

    Added the template file for the ECE major

---
+++ src/requirements/data/templates/ece.ts
@@ -0,0 +1,86 @@
+import { Template } from '@/requirements/types';
+
+const eceTemplate: Template = [
+  {
+    reqGroup: 'UNI',
+    name: 'Physical Education',
+    placeholderSemesters: [1, 2],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Mathematics',
+    placeholderSemesters: [1, 2, 3, 4],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Physics',
+    placeholderSemesters: [2, 3, 4],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Chemistry',
+    placeholderSemesters: [1],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'First-Year Writing Seminars',
+    placeholderSemesters: [1, 2],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Advanced Programming/Computer Engineering',
+    placeholderSemesters: [3],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'ENGRD 2300',
+    placeholderSemesters: [3],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Core Courses',
+    placeholderSemesters: [3, 4],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Engineering Distribution',
+    placeholderSemesters: [4],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Liberal Studies: 6 courses',
+    placeholderSemesters: [3, 4, 5, 6, 7, 8],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Outside ECE Technical Electives',
+    placeholderSemesters: [5, 6, 7],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Culminating Design Experience (CDE)',
+    placeholderSemesters: [7],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Advisor Approved Electives',
+    placeholderSemesters: [3, 4],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Foundation Courses',
+    placeholderSemesters: [5, 6],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Upper-Level ECE Electives',
+    placeholderSemesters: [6],
+  },
+  {
+    reqGroup: 'EN',
+    name: 'Upper-Level ECE Electives: 4000 level',
+    placeholderSemesters: [7, 8],
+  },
+];
+
+export default eceTemplate;
