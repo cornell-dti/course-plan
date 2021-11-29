1ee074e6dcfcdde47dc9053cc319957ce1951c8e
Will Spencer <will.e.spencer@gmail.com>
2021-11-29 11:53:37 -0500

    Create analytics container and add it to route file (copied from old branch)

---
+++ src/containers/Analytics.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="body-container">
+      <top-bar />
+      <div class="message-container">Hi</div>
+      <div class="back_to_home">
+        <a class="back_to_home_link" href="/login">Back to home</a>
+      </div>
+      <custom-footer />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CustomFooter from '@/components/Footer.vue';
+import TopBar from '@/components/TopBar.vue';
+
+export default defineComponent({
+  components: { CustomFooter, TopBar },
+});
+</script>
+
+<style scoped lang="scss">
+.back_to_home {
+  font-size: 32px;
+  vertical-align: middle;
+  text-align: center;
+  color: #7b7d7e;
+  margin-bottom: 10rem;
+  margin-top: 6rem;
+  height: 100px;
+  @media (max-width: 1154px) {
+    margin-bottom: 3rem;
+  }
+  @media (max-width: 800px) {
+    margin-top: 8%;
+    font-size: 8vw;
+    margin-bottom: 0;
+  }
+}
+a.back_to_home_link {
+  color: #7b7d7e;
+}
+.message-container {
+  display: flex;
+  margin-bottom: 3%;
+  margin-top: 10%;
+  justify-content: center;
+  @media (max-width: 1154px) {
+    margin-top: 8vh;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+.body-container {
+  min-height: 100vh;
+}
+</style>
