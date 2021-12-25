e40d16ab377e8189f6103111998bed9b307cbea9
Benjamin Shen <bfs45@cornell.edu>
2021-12-25 14:00:13 -0500

    set up structure

--- src/requirements/requirement-json-generator.ts
+++ src/requirements/requirement-json-generator.ts
@@ -9,6 +9,7 @@ import {
 import sourceRequirements from './data';
 import { NO_EQUIVALENT_COURSES_COURSE_ID, SPECIAL_COURSES } from './data/constants';
 import { fullCoursesArray } from '../assets/courses/typed-full-courses';
+import universityRequirements from './data/university';
 
 /**
  * Special (synthetic) courses used for AP/IB fulfillment.
@@ -24,6 +25,37 @@ const specialCourses: Course[] = Object.entries(SPECIAL_COURSES)
     acadGroup: '',
   }))
   .filter(({ crseId }) => crseId !== NO_EQUIVALENT_COURSES_COURSE_ID);
+
+type MutableDecoratedJson = {
+  university: {
+    [key: string]: {
+      readonly name: string;
+      readonly requirements: readonly DecoratedCollegeOrMajorRequirement[];
+    };
+  };
+  college: {
+    [key: string]: {
+      readonly name: string;
+      readonly requirements: readonly DecoratedCollegeOrMajorRequirement[];
+    };
+  };
+  major: MutableMajorRequirements<DecoratedCollegeOrMajorRequirement>;
+  minor: {
+    [key: string]: {
+      readonly name: string;
+      readonly schools: readonly string[];
+      readonly requirements: readonly DecoratedCollegeOrMajorRequirement[];
+    };
+  };
+  grad: {
+    [key: string]: {
+      readonly name: string;
+      // Unsure if grad programs can be offered by multiple schools, but allows flexibility.
+      readonly schools: readonly string[];
+      readonly requirements: readonly DecoratedCollegeOrMajorRequirement[];
+    };
+  };
+};
 
 const getEligibleCoursesFromRequirementCheckers = (
   checkers: readonly RequirementChecker[]
@@ -83,38 +115,8 @@ const decorateRequirementWithCourses = (
   }
 };
 
-const produceSatisfiableCoursesAttachedRequirementJson = (): DecoratedRequirementsJson => {
+const requirementsJsonWithSatisfiableCourses = (): MutableDecoratedJson => {
   const { university, college, major, minor, grad } = sourceRequirements;
-  type MutableDecoratedJson = {
-    university: {
-      [key: string]: {
-        readonly name: string;
-        readonly requirements: readonly DecoratedCollegeOrMajorRequirement[];
-      };
-    };
-    college: {
-      [key: string]: {
-        readonly name: string;
-        readonly requirements: readonly DecoratedCollegeOrMajorRequirement[];
-      };
-    };
-    major: MutableMajorRequirements<DecoratedCollegeOrMajorRequirement>;
-    minor: {
-      [key: string]: {
-        readonly name: string;
-        readonly schools: readonly string[];
-        readonly requirements: readonly DecoratedCollegeOrMajorRequirement[];
-      };
-    };
-    grad: {
-      [key: string]: {
-        readonly name: string;
-        // Unsure if grad programs can be offered by multiple schools, but allows flexibility.
-        readonly schools: readonly string[];
-        readonly requirements: readonly DecoratedCollegeOrMajorRequirement[];
-      };
-    };
-  };
   const decoratedJson: MutableDecoratedJson = {
     university: {},
     college: {},
@@ -122,40 +124,43 @@ const produceSatisfiableCoursesAttachedRequirementJson = (): DecoratedRequiremen
     minor: {},
     grad: {},
   };
-  const decorateRequirements = (requirements: readonly CollegeOrMajorRequirement[]) =>
+  const decorateRequirementsWithCourses = (requirements: readonly CollegeOrMajorRequirement[]) =>
     requirements.map(decorateRequirementWithCourses);
   Object.entries(university).forEach(([universityName, universityRequirement]) => {
     const { requirements, ...rest } = universityRequirement;
     decoratedJson.university[universityName] = {
       ...rest,
-      requirements: decorateRequirements(requirements),
+      requirements: decorateRequirementsWithCourses(requirements),
     };
   });
   Object.entries(college).forEach(([collegeName, collegeRequirement]) => {
     const { requirements, ...rest } = collegeRequirement;
     decoratedJson.college[collegeName] = {
       ...rest,
-      requirements: decorateRequirements(requirements),
+      requirements: decorateRequirementsWithCourses(requirements),
     };
   });
   Object.entries(major).forEach(([majorName, majorRequirement]) => {
     const { requirements, specializations, ...rest } = majorRequirement;
     decoratedJson.major[majorName] = {
       ...rest,
-      requirements: decorateRequirements(requirements),
-      specializations: decorateRequirements(specializations ?? []),
+      requirements: decorateRequirementsWithCourses(requirements),
+      specializations: decorateRequirementsWithCourses(specializations ?? []),
     };
   });
   Object.entries(minor).forEach(([minorName, minorRequirement]) => {
     const { requirements, ...rest } = minorRequirement;
     decoratedJson.minor[minorName] = {
       ...rest,
-      requirements: decorateRequirements(requirements),
+      requirements: decorateRequirementsWithCourses(requirements),
     };
   });
   Object.entries(grad).forEach(([gradName, gradRequirement]) => {
     const { requirements, ...rest } = gradRequirement;
-    decoratedJson.grad[gradName] = { ...rest, requirements: decorateRequirements(requirements) };
+    decoratedJson.grad[gradName] = {
+      ...rest,
+      requirements: decorateRequirementsWithCourses(requirements),
+    };
   });
 
   // Check no duplicate requirement identifier
@@ -183,10 +188,88 @@ const produceSatisfiableCoursesAttachedRequirementJson = (): DecoratedRequiremen
   return decoratedJson;
 };
 
-const decoratedRequirementString = JSON.stringify(
-  produceSatisfiableCoursesAttachedRequirementJson(),
-  undefined,
-  2
+const decorateRequirementWithExams = (
+  requirement: DecoratedCollegeOrMajorRequirement
+): DecoratedCollegeOrMajorRequirement => {
+  switch (requirement.fulfilledBy) {
+    case 'self-check':
+      return requirement;
+    case 'courses':
+    case 'credits': {
+      const { courses, ...rest } = requirement;
+      return {
+        ...rest,
+        courses: courses.concat([[10000000]]),
+      };
+    }
+    case 'toggleable': {
+      const { fulfillmentOptions } = requirement;
+      return {
+        ...requirement,
+        fulfillmentOptions: Object.fromEntries(
+          Object.entries(fulfillmentOptions).map(([optionName, option]) => {
+            const { courses, ...rest } = option;
+            return [optionName, { ...rest, courses: courses.concat([[20000000]]) }];
+          })
+        ),
+      };
+    }
+    default:
+      throw new Error();
+  }
+};
+
+const requirementsJsonWithSatisfiableExams = (
+  decoratedJson: MutableDecoratedJson
+): DecoratedRequirementsJson => {
+  const decorateRequirementsWithExams = (
+    requirements: readonly DecoratedCollegeOrMajorRequirement[]
+  ) => requirements.map(decorateRequirementWithExams);
+  const { university, college, major, minor, grad } = decoratedJson;
+  Object.entries(university).forEach(([universityName, universityRequirement]) => {
+    const { requirements, ...rest } = universityRequirement;
+    decoratedJson.university[universityName] = {
+      ...rest,
+      requirements: decorateRequirementsWithExams(requirements),
+    };
+  });
+  Object.entries(college).forEach(([collegeName, collegeRequirement]) => {
+    const { requirements, ...rest } = collegeRequirement;
+    decoratedJson.college[collegeName] = {
+      ...rest,
+      requirements: decorateRequirementsWithExams(requirements),
+    };
+  });
+  Object.entries(major).forEach(([majorName, majorRequirement]) => {
+    const { requirements, specializations, ...rest } = majorRequirement;
+    decoratedJson.major[majorName] = {
+      ...rest,
+      requirements: decorateRequirementsWithExams(requirements),
+      specializations: decorateRequirementsWithExams(specializations ?? []),
+    };
+  });
+  Object.entries(minor).forEach(([minorName, minorRequirement]) => {
+    const { requirements, ...rest } = minorRequirement;
+    decoratedJson.minor[minorName] = {
+      ...rest,
+      requirements: decorateRequirementsWithExams(requirements),
+    };
+  });
+  Object.entries(grad).forEach(([gradName, gradRequirement]) => {
+    const { requirements, ...rest } = gradRequirement;
+    decoratedJson.grad[gradName] = {
+      ...rest,
+      requirements: decorateRequirementsWithExams(requirements),
+    };
+  });
+
+  return decoratedJson;
+};
+
+const decoratedRequirements = requirementsJsonWithSatisfiableExams(
+  requirementsJsonWithSatisfiableCourses()
 );
 
-writeFileSync('src/requirements/decorated-requirements.json', decoratedRequirementString);
+const decoratedRequirementsString = JSON.stringify(decoratedRequirements, undefined, 2);
+
+writeFileSync('src/requirements/decorated-requirements.json', decoratedRequirementsString);
