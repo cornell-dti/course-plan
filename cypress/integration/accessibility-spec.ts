1b81615eddf46d171055d2406ede99f9f62e6e14
Will Spencer <will.e.spencer@gmail.com>
2022-03-08 16:08:25 -0500

    Add simple test on landing page and dashboard

--- cypress/integration/accessibility-spec.ts
+++ cypress/integration/accessibility-spec.ts
@@ -3,29 +3,29 @@
  * Can and should be expanded in the future.
  */
 
-// Before running tests, starts on landing page, logs in to firebase, then visits the dashboard
+// Before running tests, start on landing page, login to firebase, and inject accessibility scripts
 // Log in occurs with TEST_UID of the courseplan testing account using a function from the cypress-firebase package
-before('Visit site logged in', () => {
+before('Visit landing page logged in', () => {
   cy.visit('localhost:8080/login');
   cy.login(Cypress.env('TEST_UID'));
-  cy.visit('localhost:8080');
+
   // eslint-disable-next-line cypress/no-unnecessary-waiting
-  cy.wait(5000); // ensure the page has time to load
+  cy.wait(2000); // ensure the page has time to load
+
+  cy.injectAxe(); // inject the axe-core library to check accessibility
 });
 
-// Confirm that a semester can be added to the plan
-it('Add a semester (Fall of oldest year)', () => {
-  // open the new semester modal
-  cy.get('[data-cyId=semesterView-addSemesterButton]').click();
+it('Check landing page accessibility', () => {
+  cy.checkA11y();
+});
 
-  // click Fall
-  cy.get('[data-cyId=newSemester-seasonWrapper]').click();
-  cy.get('[data-cyId=newSemester-seasonItem]').first().click();
+it('Visit dashboard and check dashboard accessibility', () => {
+  cy.visit('localhost:8080');
 
-  // click oldest year
-  cy.get('[data-cyId=newSemester-yearWrapper]').click();
-  cy.get('[data-cyId=newSemester-yearItem]').first().click();
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.wait(5000); // ensure the page has time to load
 
-  // add semester
-  cy.get('[data-cyId=modal-button]').click();
+  cy.injectAxe(); // re-inject the library due to switching page
+
+  cy.checkA11y();
 });
