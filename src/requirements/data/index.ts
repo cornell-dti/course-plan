e9fd15377aab55cc8acc21aad8591994bb605584
Jerry Wang <yw853@cornell.edu>
2022-03-20 18:16:14 -0400

    added ineq requirements courses, need to fix jsons next

--- src/requirements/data/index.ts
+++ src/requirements/data/index.ts
@@ -8,7 +8,6 @@ import businessRequirements from './colleges/bu';
 import engineeringRequirements from './colleges/en';
 import humanEcologyRequirements from './colleges/he';
 import ilrRequirements from './colleges/il';
-
 import aemRequirements from './majors/aem';
 import asRequirements from './majors/as';
 import astroRequrements from './majors/astro';
@@ -47,7 +46,6 @@ import pamRequirements from './majors/pam';
 import physRequirements from './majors/phys';
 import spanishRequirements from './majors/spanish';
 import stsRequirements from './majors/sts';
-
 import aerospaceMinorRequirements from './minors/aerospace';
 import appliedMathMinorRequirements from './minors/applied-math';
 import buMinorRequirements from './minors/bu';
@@ -58,6 +56,7 @@ import deaMinorRequirements from './minors/dea';
 import eceMinorRequirements from './minors/ece';
 import hpMinorRequirements from './minors/hp';
 import hdMinorRequirements from './minors/hd';
+import inequalityRequirements from './minors/ineq';
 import infoENMinorRequirements from './minors/infoEN';
 import lingMinorRequirements from './minors/ling';
 import mathMinorRequirements from './minors/math';
@@ -356,6 +355,11 @@ const json: RequirementsJson = {
       name: 'Human Development',
       schools: ['HE'],
       requirements: hdMinorRequirements,
+    },
+    INEQ: {
+      name: 'Inequality Studies',
+      schools: ['AS1', 'AS2'],
+      requirements: inequalityRequirements,
     },
     ISST: {
       name: 'Information Science [Engineering]',
