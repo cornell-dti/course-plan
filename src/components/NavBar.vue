c2c2a7b766874f8bd60d24382c86b69bcc3540a3
Noah Schiff <nps39@cornell.edu>
2022-04-30 02:06:22 -0400

    turn featureFlags for tools page back on

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -104,7 +104,7 @@ import { defineComponent } from 'vue';
 import firebase from 'firebase/app';
 import { GTagEvent } from '@/gtag';
 import { clickOutside } from '@/utilities';
-// import featureFlagCheckers from '@/feature-flags';
+import featureFlagCheckers from '@/feature-flags';
 
 export default defineComponent({
   props: {
@@ -118,8 +118,7 @@ export default defineComponent({
   },
   computed: {
     toolsEnabled(): boolean {
-      return true;
-      // return featureFlagCheckers.isToolsEnabled();
+      return featureFlagCheckers.isToolsEnabled();
     },
   },
   methods: {
