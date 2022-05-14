ba97d917a23ee41f307b882318e08cf3a7e1fcb5
jerrry1123 <66176696+jerrry1123@users.noreply.github.com>
2022-05-14 12:23:01 -0400

    Add transfer credits and save confirmation modal (#691)
    * added ineq requirements courses, need to fix jsons next
    
    * finsihed changes to inequality major,
    changed requirement description to allow req-gen to run
    
    * updated requirement tests snapshot
    
    * added the first profile edit page to a new profile container
    
    * removed code that is not used so far
    
    * deleted another piece of yet unused code
    
    * fixed type error
    
    * created basic logic for toggling
     between basic info and transfer credits
    
    * fixed checks for whether the save button should be active
    
    * fixed save issue, scroll still unresolved
    
    * removed debugging alerts
    
    * fixed lint error
    
    * fixed EOF error?
    
    * Fix scroll bar disappearing bug (and others)
    
    * fixed type error?
    
    * got rid of inequality change
    
    * actually fixed stuff
    
    * fixed decorated requirements
    
    * made PR up to date with master
    
    * changed background to pure white to match textbox background
    
    * fixed narrow pages covering title
    
    * fixed save button logic issue
    
    * fixed font
    
    * implemented michaels feedback
    
    * fixed lint error
    
    * fixed prettier issue
    
    Co-authored-by: Will Spencer <will.e.spencer@gmail.com>

---
+++ src/components/Modals/ProfileConfirmation.vue
@@ -0,0 +1,32 @@
+<template>
+  <teleport-modal
+    title="Save Changes"
+    leftButtonText="Cancel"
+    rightButtonText="OK"
+    content-class="content-clear"
+    @modal-closed="closeConfirmationModal"
+    @left-button-clicked="closeConfirmationModal"
+    @right-button-clicked="submitAndClose"
+  >
+    <span>Are you sure you want to add these changes to the Profile page?</span>
+  </teleport-modal>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TeleportModal from '@/components/Modals/TeleportModal.vue';
+
+export default defineComponent({
+  components: { TeleportModal },
+  emits: { 'close-modal': () => true, 'submit-onboarding': () => false },
+  methods: {
+    closeConfirmationModal() {
+      this.$emit('close-modal');
+    },
+    submitAndClose() {
+      this.closeConfirmationModal();
+      this.$emit('submit-onboarding');
+    },
+  },
+});
+</script>
