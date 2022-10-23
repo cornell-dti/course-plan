6a888e318fbb26c5b71b1e6c4a5610d1bf7496d2
nps39 <nps39@cornell.edu>
2022-10-23 00:54:29 -0400

    fix AdvisorPackage types

--- src/requirements/tools-types.ts
+++ src/requirements/tools-types.ts
@@ -14,7 +14,7 @@ export type Advisor = {
 export type AdvisorPackage = {
   readonly name: string;
   readonly type: 'major' | 'minor' | 'college';
-  readonly source: string;
+  readonly source?: string;
   readonly acronym: string;
   readonly email: string;
 };
