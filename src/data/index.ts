568b6130766248dcfd5b72ec3f127f2757e4386e
jerrry1123 <66176696+jerrry1123@users.noreply.github.com>
2023-02-17 16:41:21 -0500

    restored applied econ

--- src/data/index.ts
+++ src/data/index.ts
@@ -75,6 +75,7 @@ import { MATH2940, CHEM2080 } from './specializations/en';
 import earthAndAtmosphericSciencesMinorRequirements, {
   easMinorAdvisors,
 } from './minors/earth-atmo';
+import appliedEconMinorRequirements, { appliedEconMinorAdvisors } from './minors/aem';
 
 const json: RequirementsJson = {
   university: {
@@ -424,6 +425,13 @@ const json: RequirementsJson = {
       requirements: aerospaceMinorRequirements,
       advisors: aerospaceMinorAdvisors,
       abbrev: 'Aero Eng',
+    },
+    APPLIEDECON: {
+      name: 'Applied Economics',
+      schools: ['AG', 'BU'],
+      requirements: appliedEconMinorRequirements,
+      advisors: appliedEconMinorAdvisors,
+      abbrev: 'Applied Econ',
     },
     APPLIEDMATH: {
       name: 'Applied Mathematics',
