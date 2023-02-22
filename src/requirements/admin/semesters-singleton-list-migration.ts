ece1901136b78b0894bd87bad0224a7c8b93e888
zachary0kent <73757337+zachary0kent@users.noreply.github.com>
2023-02-21 20:11:20 -0500

    Update src/requirements/admin/semesters-singleton-list-migration.ts

--- src/requirements/admin/semesters-singleton-list-migration.ts
+++ src/requirements/admin/semesters-singleton-list-migration.ts
@@ -14,18 +14,17 @@ async function runOnUser(userEmail: string) {
 }
 
 async function main() {
-  let userEmail = process.argv[2];
+  const userEmail = process.argv[2];
   if (userEmail != null) {
     await runOnUser(userEmail);
     return;
   }
   const collection = await usernameCollection.get();
-  const userEmails = collection.docs.map(it => it.id);
-  for (userEmail of userEmails) {
-    console.group(`Running on ${userEmail}...`);
+  for (const { id } of collection.docs) {
+    console.group(`Running on ${id}...`);
     // Intentionally await in a loop to have no interleaved console logs.
     // eslint-disable-next-line no-await-in-loop
-    await runOnUser(userEmail);
+    await runOnUser(id);
     console.groupEnd();
   }
 }
