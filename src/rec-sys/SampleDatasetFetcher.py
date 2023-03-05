db08e9c3937c500a6eba5ab756d19199912149c3
jerrry1123 <66176696+jerrry1123@users.noreply.github.com>
2023-03-05 13:25:19 -0500

    Set up NCF Model and tested it on sample dataset

---
+++ src/rec-sys/SampleDatasetFetcher.py
@@ -0,0 +1,26 @@
+from recommenders.datasets import movielens
+from recommenders.datasets.python_splitters import python_chrono_split
+
+MOVIELENS_DATA_SIZE = '100k'
+
+df = movielens.load_pandas_df(
+    size=MOVIELENS_DATA_SIZE,
+    header=["userID", "itemID", "rating", "timestamp"]
+)
+
+df.head()
+
+train, test = python_chrono_split(df, 0.75)
+
+test = test[test["userID"].isin(train["userID"].unique())]
+test = test[test["itemID"].isin(train["itemID"].unique())]
+
+leave_one_out_test = test.groupby("userID").last().reset_index()
+
+train_file = "./train.csv"
+test_file = "./test.csv"
+leave_one_out_test_file = "./leave_one_out_test.csv"
+# train.to_csv(train_file, index=False)
+# test.to_csv(test_file, index=False)
+# leave_one_out_test.to_csv(leave_one_out_test_file, index=False)
+print(type(test))
