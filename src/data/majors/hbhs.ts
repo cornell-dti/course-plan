8c8ae49b954527f1ca2797ff04d4be6dbba96b3f
Noah Schiff <47431797+noschiff@users.noreply.github.com>
2023-03-06 18:17:25 -0500

    first update to HBHS

--- src/data/majors/hbhs.ts
+++ src/data/majors/hbhs.ts
@@ -4,39 +4,46 @@ import { AdvisorGroup } from '../../tools/advisors/types';
 
 const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
   {
-    name: 'Introductory Chemistry',
-    description:
-      'Students in the major must take CHEM 2070 - CHEM 2080 or CHEM 2150 with AP credit. ' +
-      'Students, especially those on the pre-health track, are reccomended to take CHEM 2070 - CHEM 2080.',
+    name: 'HBHS Introductory Course',
+    description: 'NS 1400 Introduction to Human Biology, Health, and Society',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
-    fulfilledBy: 'toggleable',
-    fulfillmentOptions: {
-      'Option 1': {
-        description: 'CHEM 2070 AND CHEM 2080',
-        checker: includesWithSubRequirements(['CHEM 2070'], ['CHEM 2080']),
-        counting: 'courses',
-        perSlotMinCount: [1, 1],
-        slotNames: ['CHEM 2070', 'CHEM 2080'],
-      },
-      'Option 2': {
-        description: 'CHEM 2150 with AP credit',
-        checker: includesWithSubRequirements(['CHEM 2150']),
-        counting: 'courses',
-        perSlotMinCount: [1],
-        slotNames: ['Course'],
-      },
-    },
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
+    checker: includesWithSubRequirements(['NS 1400']),
+    fulfilledBy: 'courses',
+    perSlotMinCount: [1],
+    slotNames: ['Course'],
   },
   {
-    name: 'Introductory Biology Lab',
+    name: 'HBHS Survey Course',
+    description: 'NS 1150 or NS 1220',
+    source:
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
+    checker: includesWithSubRequirements(['NS 1150', 'NS 1220']),
+    fulfilledBy: 'courses',
+    perSlotMinCount: [1],
+    slotNames: ['Course'],
+  },
+  {
+    name: 'Introductory Chemistry',
+    description:
+      'Students in the major must take CHEM 2070 & CHEM 2080 or CHEM 2150 with AP Chemistry credit. ' +
+      'Students, especially those on the pre-health track, are recommended to take CHEM 2070 - CHEM 2080 and forfeit AP credit.',
+    source:
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
+    fulfilledBy: 'courses',
+    checker: includesWithSubRequirements(['CHEM 2070'], ['CHEM 2080', 'CHEM 2150']),
+    perSlotMinCount: [1, 1],
+    slotNames: ['CHEM 2070 or AP credit', 'CHEM 2080 or CHEM 2150'],
+  },
+  {
+    name: 'Introductory Biology',
     description: 'BIOG 1500 or BIOSM 1500',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     checker: includesWithSubRequirements(['BIOG 1500', 'BIOSM 1500']),
     fulfilledBy: 'courses',
     perSlotMinCount: [1],
-    slotNames: ['BIOG 1500 or BIOSM 1500'],
+    slotNames: ['BIOG 1500 or BIOSM 150'],
   },
   {
     name: 'Introductory Biology Lecture',
@@ -44,7 +51,7 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'Students must take 2 lectures coming from two of the three categories (Cell and Development, Comparative Physiology, and Ecology and Evolutionary).' +
       'Also, students may use an AP Biology score of 5 or IB HL Biology score of 7 to place out of one introductory biology lecture. Pre-health students should not use AP scores to fulfill biology requirements.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     fulfilledBy: 'toggleable',
     fulfillmentOptions: {
       'Option 1': {
@@ -67,7 +74,7 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       },
       'Option 3': {
         description:
-          'AP Credit and one lectrue in Cell and Development, Comparative Physiology, or Ecology and Evolutionary',
+          'AP Credit and one lecture in Cell and Development, Comparative Physiology, or Ecology and Evolutionary',
         checker: includesWithSubRequirements([
           'BIOMG 1350, BIOG 1440, BIOG 1445, BIOEE 1610, BIOEE 1780',
         ]),
@@ -80,13 +87,13 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
   {
     name: 'Physics',
     description:
-      'PHYS 1101 or PHYS 2207. Students interested in pre-health tracks should also take PHYS 1102 General Physics II OR PHYS 2208 Fundamentals of Physics',
+      'PHYS 1101 or PHYS 2207. Students interested in pre-health tracks should also take PHYS 1102 or PHYS 2208.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     checker: includesWithSubRequirements(['PHYS 1101', 'PHYS 2207']),
     fulfilledBy: 'courses',
     perSlotMinCount: [1],
-    slotNames: ['PHYS 1101 or PHYS 2207'],
+    slotNames: ['PHYS 1101 or PHYS 2207],
   },
   {
     name: 'Organic Chemistry Lecture',
@@ -94,7 +101,7 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'Students in the major must take one of the following sequences. Students interested in pre-health tracks should take a two-course sequence of organic chemistry lectures' +
       'Students taking a two-course sequence must take both courses in the sequence as one course alone will not fulfill the requirement.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     fulfilledBy: 'toggleable',
     fulfillmentOptions: {
       'Option 1': {
@@ -102,7 +109,7 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
         checker: includesWithSubRequirements(['CHEM 1570']),
         counting: 'courses',
         perSlotMinCount: [1],
-        slotNames: ['Course'],
+        slotNames: ['Course']
       },
       'Option 2': {
         description: 'CHEM 3530',
@@ -131,28 +138,28 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
     name: 'Organic Chemistry Lab',
     description: 'CHEM 2510 or CHEM 3010',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     checker: includesWithSubRequirements(['BIOG 2510', 'BIOSM 3010']),
     fulfilledBy: 'courses',
     perSlotMinCount: [1],
-    slotNames: ['BIOG 2510 or BIOSM 3010'],
+    slotNames: ['BIOG 2510 or BIOSM 3010']
   },
   {
     name: 'Physiology',
     description:
       'NS 3410 or BIOAP 3110. Pre-health students might also consider taking NS 3420 Human Anatomy and Physiology Lab (2 cr), which also counts toward advanced biology elective requirement',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     checker: includesWithSubRequirements(['NS 3410', 'BIOAP 3110']),
     fulfilledBy: 'courses',
     perSlotMinCount: [1],
-    slotNames: ['NS 3410 or BIOAP 3110'],
+    slotNames: ['NS 3410 or BIOAP 3110']
   },
   {
     name: 'Biochemistry',
     description: 'Students in the major must take one of the following sequences.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     fulfilledBy: 'toggleable',
     fulfillmentOptions: {
       'Option 1': {
@@ -160,7 +167,7 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
         checker: includesWithSubRequirements(['NS 3200']),
         counting: 'courses',
         perSlotMinCount: [1],
-        slotNames: ['Course'],
+        slotNames: ['Course']
       },
       'Option 2': {
         description: 'BIOMG 3300',
@@ -207,29 +214,24 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'Course work taken for Biology Electives may not also count for Biochemistry or HBHS Selectives. May not include Special Studies or' +
       'independent research credits (e.g., NS 4000, 4010, 4020, 4030, or 4990 or non-DNS equivalent). If a student takes an elective in Biochemistry, the following classes do not count: BIOMG 3300, 3310, or 3320, 3350, or NS 3200',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
-    // Unsure how to check for this
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     checker: [
       (course: Course): boolean =>
+        !courseMatchesCodeOptions(course, ['NS 4000']) &&
+        !courseMatchesCodeOptions(course, ['NS 4010']) &&
+        !courseMatchesCodeOptions(course, ['NS 4010']) &&
+        !courseMatchesCodeOptions(course, ['NS 4020']) &&
+        !courseMatchesCodeOptions(course, ['NS 4030']) &&
+        !courseMatchesCodeOptions(course, ['NS 4990']) &&
         !courseMatchesCodeOptions(course, ['BIOMG 3300']) &&
         !courseMatchesCodeOptions(course, ['BIOMG 3310']) &&
         !courseMatchesCodeOptions(course, ['BIOMG 3320']) &&
         !courseMatchesCodeOptions(course, ['BIOMG 3350']) &&
-        !courseMatchesCodeOptions(course, ['NS 3200']),
+        !courseMatchesCodeOptions(course, ['NS 3200'])
     ],
     checkerWarning: 'We do not check that the courses are approved Biology Electives.',
     fulfilledBy: 'credits',
     perSlotMinCount: [6],
-  },
-  {
-    name: 'Survey Course',
-    description: 'NS 1400',
-    source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
-    checker: includesWithSubRequirements(['NS 1400']),
-    fulfilledBy: 'courses',
-    perSlotMinCount: [1],
-    slotNames: ['Course'],
   },
   {
     name: 'Social Science Perspective on Health Selectives',
@@ -240,12 +242,12 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'education/counseling related to health are included in this category. See the Requirements for HBHS majors for regular updates to course' +
       'options and information; new options are available to all class years.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     // Unsure how to check for this
     checker: [],
     checkerWarning: 'We do not check that the courses are approved electives.',
     fulfilledBy: 'credits',
-    perSlotMinCount: [6],
+    perSlotMinCount: [6]
   },
   {
     name: 'Natural Science Perspective on Health Selectives',
@@ -256,12 +258,12 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'work taken for HBHS Selectives may not also count for Biology Electives. See the Requirements for HBHS majors for regular updates to' +
       'course options and information; new options are available to all class years.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     // Unsure how to check for this
     checker: [],
     checkerWarning: 'We do not check that the courses are approved electives.',
     fulfilledBy: 'credits',
-    perSlotMinCount: [6],
+    perSlotMinCount: [6]
   },
   {
     name: 'Nutritional Science Perspective on Health Selectives',
@@ -271,40 +273,40 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'for HBHS Selectives may not also count for Biology Electives. See the Requirements for HBHS majors for regular updates to course options' +
       'and information; new options are available to all class years.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     // Unsure how to check for this
     checker: [],
     checkerWarning: 'We do not check that the courses are approved electives.',
     fulfilledBy: 'credits',
-    perSlotMinCount: [3 || 4],
+    perSlotMinCount: [3 || 4]
   },
   {
     name: 'Social Sciences',
     description:
       'Choose one course in any two of the following four areas: Anthropology, Economics, Psychology, and Sociology',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     // Unsure how to check for this
     checker: [],
     checkerWarning:
       'We do not check that the courses are approved electives. We also do not check whether they come from two of the four areas.',
     fulfilledBy: 'credits',
-    perSlotMinCount: [6],
+    perSlotMinCount: [6]
   },
   {
     name: 'Humanities',
     description:
       'Choose any course with the Course Distribution Historical Analysis (HA), Literature and the Arts (LA), or Cultural Analysis (CA).',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     checker: [
       (course: Course): boolean =>
         (course.catalogDistr?.includes('CA-') ?? false) ||
         (course.catalogDistr?.includes('HA-') ?? false) ||
-        (course.catalogDistr?.includes('LA-') ?? false),
+        (course.catalogDistr?.includes('LA-') ?? false)
     ],
     fulfilledBy: 'credits',
-    perSlotMinCount: [3 || 4],
+    perSlotMinCount: [3 || 4]
   },
   {
     name: 'Calculus/Advanced Math',
@@ -313,11 +315,11 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'or Statistics at Cornell unless they have earned a score of 4 or 5 on the BC' +
       'Calculus AP Exam. Students in this case may use AP credit for both Calculus/Advanced Math and Statistics.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     checker: includesWithSubRequirements(['MATH 1105', 'MATH 1106', 'MATH 1110', 'MATH 1120']),
     fulfilledBy: 'courses',
     perSlotMinCount: [1],
-    slotNames: ['MATH 1105, MATH 1106, MATH 1110, or MATH 1120'],
+    slotNames: ['MATH 1105, MATH 1106, MATH 1110, or MATH 1120']
   },
   {
     name: 'Statistics',
@@ -326,7 +328,7 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'or Statistics at Cornell unless they have earned a score of 4 or 5 on the BC' +
       'Calculus AP Exam. Students in this case may use AP credit for both Calculus/Advanced Math and Statistics.',
     source:
-      'https://www.human.cornell.edu/sites/default/files/Academics/Registrar/Curriculum%20sheets/2021-2022/HBHS%20Curriculum%20Sheet%202021-2022.pdf',
+      'https://www.human.cornell.edu/sites/default/files/DNS/HBHS/HBHS%20Curriculum%20Sheet%202022-2023%20(1).pdf',
     checker: includesWithSubRequirements([
       'STSCI 2150',
       'PAM 2100',
@@ -336,7 +338,7 @@ const hbhsRequirements: readonly CollegeOrMajorRequirement[] = [
       'STSCI 2100',
       'MATH 1710',
       'PSYCH 2500',
-      'SOC 3010',
+      'SOC 3010'
     ]),
     fulfilledBy: 'courses',
     perSlotMinCount: [1],
