6f0da84880739c8cc03c10726c6479fcdf929f3c
Miranda Yu <mwy9@cornell.edu>
2023-03-17 15:35:55 -0400

    Parsed Other Yes List

---
+++ scripts/other-yes-list/parse.ts
@@ -0,0 +1,80 @@
+import { genTextContextMatrix } from 'pdf-to-excel';
+import { readFile } from 'fs/promises';
+// import { readFileSync } from 'fs';
+
+// Original text matrix
+// const text = readFileSync('./ApprovedLiberalStudies.pdf');
+// genTextContextMatrix(text).then((table: any) => console.log(table.slice(22,100)));
+
+export type Course = {
+	/** Corresponds to the "prefix" column on the PDF */
+	subject: string;
+  /** Corresponds to the "code" column on the PDF */
+  catalogNbr: number;
+	/** The liberal studies categories satisfied by the course */
+  categories: readonly string[];
+}
+ 
+// parses PDF into matrix
+export async function getPDF()  {
+  const pdfPath = await readFile('./ApprovedLiberalStudies.pdf');
+  return genTextContextMatrix(pdfPath);
+}
+
+// creates list of course entries
+export function genCourseEntries(data) {
+  // eslint-disable-next-line prefer-const
+  let courseEntries: Course[] = [];
+  let col = 'course prefix';
+  let subject = '';
+  let catalogNbr = 0;
+  let categories = [];
+
+  // find the index of the first course (right after Notes)
+  let start = 0;
+  for (let i = 0; i < data.length; i++) {
+    try { 
+      const v = data[i][0].str.trim();
+      if (v.slice(0,5) === 'Notes') {
+        start = i+1;
+      }
+    } catch {
+      // in case there is no list of approved courses
+    }
+  };
+  // assumes that the columns are ordered and there are no empty cells
+  for (let i = start; i < data.length; i++) {
+    try {
+      const v = data[i][0].str.trim();
+      if (col === 'course prefix' && v.length <= 5) {
+        subject = v;
+        col = 'course number';
+      } else if (col === 'course number') {
+        catalogNbr = parseInt(v, 10);
+        col = 'course title';
+      } else if (col === 'course title') {
+        // title = v;
+        col = 'college';
+      } else if (col === 'college') {
+        // college = v;
+        col = 'category';
+      } else if (col === 'category') {
+        categories = v.split(', ');
+        col = 'credits';
+      } else if (col === 'credits') {
+        // let creditList = v.split('-');
+        // creditList = creditList.map((x: string) => parseInt(x, 10));
+        // credits = creditList;
+        col = 'course prefix';
+
+        const course = { subject, catalogNbr, categories};
+        courseEntries.push(course);
+      }
+    } catch {
+      // If any data is invalid, exclude the Course from the result.
+    }
+  }
+  return courseEntries.filter(course => !Number.isNaN(course.catalogNbr));
+}
+
+// getPDF().then(pdf => console.log(genCourseEntries(pdf)));
