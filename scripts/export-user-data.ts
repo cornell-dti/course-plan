844396feacd3b69d5a8cf079944fe2fd58e05ea8
jerrry1123 <66176696+jerrry1123@users.noreply.github.com>
2023-04-13 14:14:42 -0400

    fixed lint issues

--- scripts/export-user-data.ts
+++ scripts/export-user-data.ts
@@ -1,18 +1,22 @@
-/* eslint-disable no-await-in-loop */
-import { semestersCollection } from './firebase-config';
 import fs from 'fs';
+import { semestersCollection } from './firebase-config';
 
 class Rating {
   userId: number;
+
   courseId: number;
+
   rating: number;
+
   timestamp: number;
+
   constructor(id: number, course: number, rating: 1, timestamp: 0) {
     this.userId = id;
     this.courseId = course;
     this.rating = rating;
     this.timestamp = timestamp;
   }
+
   saveAsCSV() {
     const csv = `${this.userId},${this.courseId},${this.rating},${this.timestamp}\n`;
     try {
@@ -26,10 +30,10 @@ class Rating {
 function getUserCourses(
   user: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>
 ): number[] {
-  let userCourseIDs: number[] = [];
+  const userCourseIDs: number[] = [];
   const semesters = user.get('semesters');
   for (const semester of semesters) {
-    const courses = semester.courses;
+    const { courses } = semester;
     for (const course of courses) {
       userCourseIDs.push(course.crseId);
     }
@@ -49,17 +53,15 @@ const exportUserData = async () => {
   const userSnapshot = await semestersCollection.get();
   createCSVHeaders();
   for (const user of userSnapshot.docs) {
-    let courses = getUserCourses(user);
-    let id = user.get('id');
-    if (id == undefined) {
-      continue;
-    }
-    for (const course of courses) {
-      if (course == undefined) {
-        continue;
+    const courses = getUserCourses(user);
+    const id = user.get('id');
+    if (id !== undefined) {
+      for (const course of courses) {
+        if (course !== undefined) {
+          const rating = new Rating(id, course, 1, 0);
+          rating.saveAsCSV();
+        }
       }
-      let rating = new Rating(id, course, 1, 0);
-      rating.saveAsCSV();
     }
   }
 };
