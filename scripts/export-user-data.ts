c316bc3d6169d17c7ff50e8a6d776959fc4ad9a8
jerrry1123 <66176696+jerrry1123@users.noreply.github.com>
2023-04-13 14:14:42 -0400

    fixed lint issues

--- scripts/export-user-data.ts
+++ scripts/export-user-data.ts
@@ -1,44 +1,19 @@
 import fs from 'fs';
 import { semestersCollection } from './firebase-config';
 
-<<<<<<< HEAD
 function writeToRatingsCSV(userId: number, crseId: number, rating: number, timestamp: number) {
   const csv = `${userId},${crseId},${rating},${timestamp}\n`;
   try {
     fs.appendFileSync('src/rec-sys/ratings.csv', csv);
   } catch (err) {
     console.error(err);
-=======
-class Rating {
-  userId: number;
-
-  courseId: number;
-
-  rating: number;
-
-  timestamp: number;
-
-  constructor(id: number, course: number, rating: 1, timestamp: 0) {
-    this.userId = id;
-    this.courseId = course;
-    this.rating = rating;
-    this.timestamp = timestamp;
-  }
-  saveAsCSV() {
-    const csv = `${this.userId},${this.courseId},${this.rating},${this.timestamp}\n`;
-    try {
-      fs.appendFileSync('src/rec-sys/ratings.csv', csv);
-    } catch (err) {
-      console.error(err);
-    }
->>>>>>> 21dc8df5 (added script to get user data)
   }
 }
 
 function getUserCourses(
   user: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>
 ): number[] {
-  let userCourseIDs: number[] = [];
+  const userCourseIDs: number[] = [];
   const semesters = user.get('semesters');
   for (const { courses } of semesters) {
     for (const { crseId } of courses) {
