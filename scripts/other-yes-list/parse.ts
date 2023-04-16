ac5a9b26b41f149ea1761ec12543aae57d9b3694
Miranda Yu <mwy9@cornell.edu>
2023-04-16 14:12:24 -0400

    removed try catch

--- scripts/other-yes-list/parse.ts
+++ scripts/other-yes-list/parse.ts
@@ -1,3 +1,5 @@
+import { fullCoursesArray } from '../../src/assets/courses/typed-full-courses';
+
 export type Course = {
   /** Corresponds to the "prefix" column on the PDF */
   subject: string;
@@ -5,6 +7,10 @@ export type Course = {
   catalogNbr: number;
   /** The liberal studies categories satisfied by the course */
   categories: readonly string[];
+};
+
+export type CourseWithId = Course & {
+  crseId: number;
 };
 
 enum Col {
@@ -27,52 +33,59 @@ export function genCourseEntries(data) {
   // find the index of the first course (right after Notes)
   let start = 0;
   for (let i = 0; i < data.length; i += 1) {
-    try {
-      const v = data[i][0].str.trim();
-      if (v.slice(0, 5) === 'Notes') {
-        start = i + 1;
-      }
-    } catch {
-      // in case there is no list of approved courses
+    const v = data[i][0].str.trim();
+    if (v.slice(0, 5) === 'Notes') {
+      start = i + 1;
     }
   }
   // assumes that the columns are ordered and there are no empty cells
   for (let i = start; i < data.length; i += 1) {
-    try {
-      const v = data[i][0].str.trim();
-      switch (c) {
-        case Col.CoursePrefix:
-          if (v.length <= 5) {
-            subject = v;
-            c = Col.CourseNumber;
-          }
-          break;
-        case Col.CourseNumber:
-          catalogNbr = parseInt(v, 10);
-          c = Col.Title;
-          break;
-        case Col.Title:
-          c = Col.College;
-          break;
-        case Col.College:
-          c = Col.Category;
-          break;
-        case Col.Category:
-          categories = v.split(', ');
-          c = Col.Credits;
-          break;
-        case Col.Credits: {
-          const course = { subject, catalogNbr, categories };
-          courseEntries.push(course);
-          c = Col.CoursePrefix;
-          break;
+    const v = data[i][0].str.trim();
+    switch (c) {
+      case Col.CoursePrefix:
+        if (v.length <= 5) {
+          subject = v;
+          c = Col.CourseNumber;
         }
-        default:
-          break;
+        break;
+      case Col.CourseNumber:
+        catalogNbr = parseInt(v, 10);
+        c = Col.Title;
+        break;
+      case Col.Title:
+        c = Col.College;
+        break;
+      case Col.College:
+        c = Col.Category;
+        break;
+      case Col.Category:
+        categories = v.split(', ');
+        c = Col.Credits;
+        break;
+      case Col.Credits: {
+        const course = { subject, catalogNbr, categories };
+        courseEntries.push(course);
+        c = Col.CoursePrefix;
+        break;
       }
-    } catch {
-      // If any data is invalid, exclude the Course from the result.
+      default:
+        break;
     }
   }
   return courseEntries.filter(course => !Number.isNaN(course.catalogNbr));
+}
+
+export function getValidCourses(data) {
+  const courseEntries = genCourseEntries(data);
+  const validCourses: CourseWithId[] = [];
+
+  for (const { subject, catalogNbr, crseId } of fullCoursesArray) {
+    for (const course of courseEntries) {
+      if (subject === course.subject && catalogNbr === course.catalogNbr.toString()) {
+        validCourses.push({ ...course, crseId });
+        break;
+      }
+    }
+  }
+  return validCourses;
 }
