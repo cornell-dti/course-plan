f8061ec20af48268d5c8860e54fa0c83683f811a
elizabeth-tang <43019442+elizabeth-tang@users.noreply.github.com>
2023-04-16 10:37:12 -0700

    Multiple plans dropdown (#806)
    * Implement dropdown frontend
    
    - Add dropdown component
    - Add cypress tests for open/close states.
    
    * Add changing arrow direction
    
    * Change plan when clicked
    
    * Run checks
    
    * Enable feature flag before running test
    
    * Fix arrow resizing problem and address Zak changes
    
    * Change colors to set colors, try to fix test
    
    * Tests working + remove feature flag for time being
    
    * Finally passing the cypress tests <33
    
    * Hover state cursor -> pointer
    
    * Refactor css into scss
    
    * Add scss file lol

---
+++ src/components/Requirements/MultiplePlansDropdown.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="multiplePlans-dropdown">
+      <div
+        class="multiplePlans-dropdown-placeholder wrapper"
+        @click="closeDropdownIfOpen()"
+        data-cyId="multiplePlans-dropdown"
+      >
+        <img :src="editPlan" class="multiplePlans-dropdown-placeholder editimg" alt="edit plans" />
+        <div class="multiplePlans-dropdown-placeholder plan">{{ currPlan }}</div>
+        <img
+          class="multiplePlans-dropdown-placeholder up-arrow"
+          v-if="shown"
+          alt="close dropdown"
+        />
+        <img class="multiplePlans-dropdown-placeholder down-arrow" v-else alt="open dropdown" />
+      </div>
+      <div
+        class="multiplePlans-dropdown-content"
+        v-if="shown"
+        data-cyId="multiplePlans-dropdown-content"
+        :class="shown"
+      >
+        <div
+          v-for="otherPlan in otherPlans"
+          class="multiplePlans-dropdown-content item"
+          :key="otherPlan"
+          @click="planClicked(otherPlan)"
+        >
+          {{ otherPlan }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import editPlan from '@/assets/images/editplan.svg';
+
+export default defineComponent({
+  props: {
+    plan: { type: String, default: 'No additional plans yet' },
+  },
+  data() {
+    return {
+      plans: ['PLAN 1', 'PLAN 2', 'PLAN 3', 'PLAN 4'],
+      shown: false,
+      currPlan: 'PLAN 1',
+      editPlan,
+    };
+  },
+  computed: {
+    otherPlans() {
+      const filtered = this.plans.filter(plan => plan !== this.currPlan);
+      return filtered.length === 0 ? ['No additional plans yet'] : filtered;
+    },
+  },
+  methods: {
+    closeDropdownIfOpen() {
+      this.shown = !this.shown;
+    },
+    planClicked(plan: string) {
+      if (this.plans.length !== 1) this.currPlan = plan;
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+@import '@/components/Requirements/MultiplePlansDropdown.scss';
+</style>
