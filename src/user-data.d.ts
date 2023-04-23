266f91e089bec439c2ddde43114bcba578527dda
elizabeth-tang <43019442+elizabeth-tang@users.noreply.github.com>
2023-04-23 07:22:18 -0700

    migration script for multiple plans in firebase (#817)
    * migration script for multiple plans in firebase
    
    * Move Plan type to Migration Script

--- src/user-data.d.ts
+++ src/user-data.d.ts
@@ -36,7 +36,7 @@ type FirestoreSemester = {
 };
 
 type FirestoreSemestersData = {
-  readonly plans: readonly Plan[];
+  readonly plans?: readonly Plan[];
   readonly semesters: readonly FirestoreSemester[];
   readonly orderByNewest: boolean;
 };
@@ -177,6 +177,11 @@ interface FullClassRosterCourseWithUniqueID extends CornellCourseRosterCourseFul
   readonly userChosenCredits: number;
   readonly uniqueID: number;
 }
+
+type Plan = {
+  readonly name: string;
+  readonly semesters: readonly FirestoreSemester[];
+};
 
 // college and grad are optional fields: grad can be undefined if the user hasn't selected a grad program, and college can be undefined if the user has only selected a grad program.
 type AppOnboardingData = {
