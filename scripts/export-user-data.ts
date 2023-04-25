c7aff2b7783087c3946fbad8959780c1ed7017ac
jerrry1123 <66176696+jerrry1123@users.noreply.github.com>
2023-04-25 19:56:44 -0400

    removed ratings class from script

--- scripts/export-user-data.ts
+++ scripts/export-user-data.ts
@@ -1,29 +1,12 @@
 import fs from 'fs';
 import { semestersCollection } from './firebase-config';
 
-class Rating {
-  userId: number;
-
-  courseId: number;
-
-  rating: number;
-
-  timestamp: number;
-
-  constructor(id: number, course: number, rating: number, timestamp: number) {
-    this.userId = id;
-    this.courseId = course;
-    this.rating = rating;
-    this.timestamp = timestamp;
-  }
-
-  saveAsCSV() {
-    const csv = `${this.userId},${this.courseId},${this.rating},${this.timestamp}\n`;
-    try {
-      fs.appendFileSync('src/rec-sys/ratings.csv', csv);
-    } catch (err) {
-      console.error(err);
-    }
+function writeToCSV(userId: number, crseId: number, rating: number, timestamp: number) {
+  const csv = `${userId},${crseId},${rating},${timestamp}\n`;
+  try {
+    fs.appendFileSync('src/rec-sys/ratings.csv', csv);
+  } catch (err) {
+    console.error(err);
   }
 }
 
@@ -57,8 +40,7 @@ const exportUserData = async () => {
     const courses = getUserCourses(user);
     for (const course of courses) {
       if (course !== undefined) {
-        const rating = new Rating(id, course, 1, 0);
-        rating.saveAsCSV();
+        writeToCSV(id, course, 1, 0);
       }
     }
     id += 1;
