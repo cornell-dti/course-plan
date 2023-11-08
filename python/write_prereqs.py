d8f82ae637ef61b485031b47018e965e60b3b7db
Andrew J Xu <ajx8@cornell.edu>
2023-11-08 11:23:24 -0500

    script

--- python/write_prereqs.py
+++ python/write_prereqs.py
@@ -2,8 +2,6 @@ from read_prereqs import get_all_classes_req_strs_from_semester
 from prereq_parse import parse_prereq_coreq_string
 from firebase_config import (
     courses_collection,
-    available_rosters_for_course_collection,
-    crse_id_to_catalog_nbr_collection,
 )
 
 
@@ -12,3 +10,12 @@ def write_prereqs(semester):
     for course in req_str_dict:
         raw_str = req_str_dict[course]
         (prereqs, coreqs) = parse_prereq_coreq_string(raw_str)
+        subject = course.split(" ")[0]
+        course_number = course.split(" ")[1]
+        course_ref = (
+            courses_collection.document(semester)
+            .document(subject)
+            .document(course_number)
+        )
+        course_ref.update({"prereqs": prereqs})
+        course_ref.update({"coreqs": coreqs})
