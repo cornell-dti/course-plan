0c7c2302844e7f345831c45f19663f2c40f21cb5
Elizabeth Tang <eliza.n.tang@gmail.com>
2024-02-11 19:41:17 -0500

    heading for saved courses

---
+++ src/components/SavedCourses/SavedCoursesHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="requirementheader">
+    <h1 class="top header-title">Collections</h1>
+    <div class="collection">
+      <drop-down-arrow class="collection-dropdown" :fillColor="darkGray" />
+      <div class="collection-name">All</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import DropDownArrow from '@/components/DropDownArrow.vue';
+
+export default defineComponent({
+  components: { DropDownArrow },
+  props: {},
+});
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables.scss';
+
+.top {
+  margin: 1.5rem 0 1rem 0;
+
+  &-small {
+    margin: 0px;
+  }
+}
+
+.header-title {
+  color: $darkGray;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+}
+
+.collection-name {
+  display: inline-block;
+  position: relative;
+  left: 0.5rem;
+  color: $darkGray;
+}
+</style>
