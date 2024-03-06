32f7b7d70ebf7b2f87ce7e0fc24a46704be7e135
Hannah Zhou <hannahyunzizhou@gmail.com>
2024-03-06 13:52:56 -0500

    Added Physical Science Core for major

--- src/data/majors/vien.ts
+++ src/data/majors/vien.ts
@@ -1,15 +1,25 @@
-import { Course, CollegeOrMajorRequirement } from '../../requirements/types';
-import { ifCodeMatch, includesWithSubRequirements } from '../../requirements/checkers';
+import { CollegeOrMajorRequirement } from '../../requirements/types';
+import { includesWithSubRequirements } from '../../requirements/checkers';
 import { AdvisorGroup } from '../../tools/advisors/types';
 
 // from here
-
-const vienScienceRequirement: readonly string[] = ['PBS', 'PBSS', 'OPHLS', 'BIOLS', 'BIO'];
-
-const vienRequirements: readonly CollegeOrMajorRequirement[] = [];
+const vienRequirements: readonly CollegeOrMajorRequirement[] = [
+  {
+    name: 'Physical Science Core',
+    description: 'some description',
+    source: 'https://courses.cornell.edu/preview_program.php?catoid=55&poid=28379#Coursework',
+    checker: includesWithSubRequirements(
+      ['BIOMG 1350', 'BIOG 1140', 'BIOG 1440', 'BIOEE 1610', 'BIOEE 1780'],
+      ['PLSCI 1420', 'BIOMI 2900']
+    ),
+    fulfilledBy: 'courses',
+    perSlotMinCount: [1, 2],
+    slotNames: ['Introductory biology courses', 'Required Biological Science Courses'],
+  },
+];
 
 export default vienRequirements;
 
 export const vienAdvisors: AdvisorGroup = {
-  advisors: [{ name: '', email: '' }],
+  advisors: [{ name: 'John Doe', email: 'johndoe@cornell.edu' }],
 };
