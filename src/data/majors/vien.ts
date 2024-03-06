00b81765c2ad9699cc9a755555a7ec2792bf6a9c
Hannah Zhou <hannahyunzizhou@gmail.com>
2024-03-06 14:20:00 -0500

    fixed physical science core requirements logic

--- src/data/majors/vien.ts
+++ src/data/majors/vien.ts
@@ -10,11 +10,12 @@ const vienRequirements: readonly CollegeOrMajorRequirement[] = [
     source: 'https://courses.cornell.edu/preview_program.php?catoid=55&poid=28379#Coursework',
     checker: includesWithSubRequirements(
       ['BIOMG 1350', 'BIOG 1140', 'BIOG 1440', 'BIOEE 1610', 'BIOEE 1780'],
-      ['PLSCI 1420', 'BIOMI 2900']
+      ['PLSCI 1420'],
+      ['BIOMI 2900']
     ),
     fulfilledBy: 'courses',
-    perSlotMinCount: [1, 2],
-    slotNames: ['Introductory biology courses', 'Required Biological Science Courses'],
+    perSlotMinCount: [1, 1, 1],
+    slotNames: ['Introductory biology courses', 'PLSCI 1420', 'BIOMI 2900'],
   },
   {
     name: 'Chemistry',
