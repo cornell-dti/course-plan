604472b1c05763b5eadd683c8d4f3bedeb089cd8
elizabeth-tang <eliza.n.tang@gmail.com>
2024-03-11 04:15:48 -0400

    frontend fixes

--- src/components/Modals/MultiplePlans/TextInputModal.vue
+++ src/components/Modals/MultiplePlans/TextInputModal.vue
@@ -7,6 +7,7 @@
           class="textInput-userinput"
           maxlength="15"
           v-model="planName"
+          :placeholder="placeholderPlan"
           @input="rightPlanClicked"
           @keydown.delete="rightPlanClicked"
         />
@@ -24,6 +25,7 @@ import store from '@/store';
 export default defineComponent({
   props: {
     label: { type: String, default: '' },
+    placeholderPlan: { type: String, default: '' },
   },
   components: { TeleportModal },
   emits: {
@@ -66,6 +68,9 @@ export default defineComponent({
     width: 100%;
     height: 2rem;
     padding-left: 0.5rem;
+    &::placeholder {
+      color: $darkPlaceholderGray;
+    }
   }
 }
 
