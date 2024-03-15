ac7f50dc0764fa9d7687da4219816ebe4843e348
elizabeth-tang <eliza.n.tang@gmail.com>
2024-03-15 02:56:15 -0400

    refactoring again

--- src/components/Modals/MultiplePlans/NamePlanModal.vue
+++ src/components/Modals/MultiplePlans/NamePlanModal.vue
@@ -78,7 +78,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-@import '@/components/Modals/PlanModalDropdown.scss';
+@import '@/components/Modals/MultiplePlans/PlanModalDropdown.scss';
 
 .content-plan {
   width: 20rem;
