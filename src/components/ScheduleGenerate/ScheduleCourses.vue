d63c639102b4c6c29f75196baf0aba7aae852767
Andrew Xu <53061040+andxu282@users.noreply.github.com>
2024-03-18 20:01:15 -0400

    [Schedule Generator] Courses Component (#893)
    * link icon w/header
    
    * fix: style schedule-gen sidebar background and padding
    
    * add schedules and courses sections
    
    * fix indent
    
    * chore: flesh out component for LHS of schedgen modal
    
    * fix
    
    * fix modal
    
    * fixes
    
    * fix
    
    ---------
    
    Co-authored-by: Simon Ilincev <trilogicworlds@gmail.com>

---
+++ src/components/ScheduleGenerate/ScheduleCourses.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="generated-schedule-schedule">
+    <div class="parent-info-div">
+      <h3 class="info-subheader">
+        <span class="info-subheader-heavy">{{ numCredits }}</span
+        >&nbsp;Credits
+      </h3>
+      <h3 class="info-subheader">
+        <span class="info-subheader-heavy">{{ classes.length }}</span
+        >&nbsp;{{ classes.length === 1 ? 'Course' : 'Courses' }}
+      </h3>
+    </div>
+    <div class="requirement-between-justifier">
+      <div v-for="cls in classes" :key="cls.name">
+        <h4 class="requirement-title-text">{{ cls.title }}</h4>
+        <div class="class-circle" :style="'border-color: ' + cls.color">
+          <span class="class-text">{{ cls.name }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    numCredits: {
+      type: Number,
+      required: true,
+    },
+    classes: {
+      type: Array as PropType<Array<{ color: string; title: string; name: string }>>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables.scss';
+.generated-schedule {
+  &-schedule {
+    border-color: $inactiveGray;
+    border: 1px solid $inactiveGray;
+    box-sizing: border-box;
+    border-radius: 4px;
+
+    padding: 2rem calc(28px + 10px) 2rem;
+  }
+}
+
+.parent-info-div {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 24px;
+}
+
+.info-subheader {
+  font-size: 16px;
+  color: $black;
+  font-style: normal;
+  line-height: normal;
+}
+
+.info-subheader-heavy {
+  font-weight: 600;
+}
+
+.requirement-title-text {
+  color: $lightPlaceholderGray;
+  font-size: 14px;
+  font-weight: 400;
+}
+
+.requirement-between-justifier > div:not(:first-child) {
+  padding-top: 21px;
+  border-top: 1px solid rgba(196, 196, 196, 0.4);
+}
+
+.requirement-between-justifier > div:not(:last-child) {
+  margin-bottom: 21px;
+}
+
+.class {
+  &-circle {
+    border-radius: 6px;
+    border: 1px solid;
+    height: 33px;
+    display: flex;
+    align-items: center;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
+  }
+
+  &-text {
+    color: $primaryGray;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    margin-left: 8px;
+    margin-right: 15px;
+  }
+}
+</style>
