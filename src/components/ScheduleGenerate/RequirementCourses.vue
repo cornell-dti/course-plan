bfa704bd5811b4c16dfdfd1f10b93940ee73edf6
Andrew Xu <53061040+andxu282@users.noreply.github.com>
2024-04-16 12:41:10 -0400

    [Schedule Generator] Add Requirements (#905)
    * add requirements component
    
    * fix dropdown
    
    * fixed dropdowns
    
    * fixed dropdowns
    
    * fixed dropdowns
    
    * fix lint
    
    * clean up styling
    
    * frontend for requirement courses (#914)
    
    * frontend for requirement courses
    
    * type fix
    
    * documentation
    
    ---------
    
    Co-authored-by: elizabeth-tang <43019442+elizabeth-tang@users.noreply.github.com>

---
+++ src/components/ScheduleGenerate/RequirementCourses.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="requirement">
+    <div class="requirement-header">
+      <button
+        class="requirement-dropdown"
+        type="button"
+        @click="showDropdown = !showDropdown"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        :aria-expanded="showDropdown"
+      >
+        <div class="requirement-header">
+          <drop-down-arrow :isFlipped="showDropdown" :fillColor="emGreen" />
+          <p v-if="selectedRequirement === ''" class="requirement-header-text">New Requirement</p>
+          <p v-else class="requirement-header-text">
+            {{ selectedRequirement }}
+          </p>
+        </div>
+      </button>
+      <button class="courseMenu-section full-opacity-on-hover" @click="deleteRequirement">
+        <div class="courseMenu-left">
+          <img src="@/assets/images/trash.svg" alt="delete req trashcan icon" />
+        </div>
+      </button>
+    </div>
+    <div v-if="showDropdown">
+      <p class="requirement-header-subtext">Please select a major requirement.</p>
+      <all-requirements-dropdown
+        :available-choices="availableRequirements"
+        :choice="selectedRequirement"
+        @on-select="selectRequirement"
+      />
+      <!-- TODO: filter course showing to ones that fulfill req -->
+      <new-course-modal
+        @close-course-modal="closeCourseModal"
+        v-if="isCourseModalOpen"
+        @add-course="addCourse"
+      />
+      <div class="requirement-courses">
+        <div v-for="c in courses" :key="c.crseId">
+          <div class="requirement-courseWrapper">
+            <course
+              :courseObj="c"
+              :active="false"
+              :isReqCourse="false"
+              :compact="false"
+              :isSchedGenCourse="true"
+              @delete-course="deleteCourse"
+            />
+          </div>
+        </div>
+        <add-course-button
+          v-if="selectedRequirement !== ''"
+          :compact="false"
+          :should-clear-padding="true"
+          @click="openCourseModal"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import AddCourseButton from '@/components/AddCourseButton.vue';
+import NewCourseModal from '@/components/Modals/NewCourse/NewCourseModal.vue';
+import { emGreen } from '@/assets/constants/scss-variables';
+import DropDownArrow from '@/components/DropDownArrow.vue';
+import AllRequirementsDropdown from '@/components/ScheduleGenerate/AllRequirementsDropdown.vue';
+import Course from '@/components/Course/Course.vue';
+import { cornellCourseRosterCourseToFirebaseSemesterCourseWithCustomIDAndColor } from '@/user-data-converter';
+import store from '@/store';
+
+export default defineComponent({
+  props: {
+    availableRequirements: {
+      type: Object as PropType<Readonly<Record<string, string>>>,
+      required: true,
+    },
+    selectedRequirement: {
+      type: String,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+  emits: {
+    'delete-available-requirement': (requirement: string) => typeof requirement === 'string',
+    'select-requirement': (requirement: string, index: number) =>
+      typeof requirement === 'string' && typeof index === 'number',
+    'add-available-requirement': (requirement: string) => typeof requirement === 'string',
+    'delete-requirement': () => true,
+  },
+  data() {
+    const courses: FirestoreSemesterCourse[] = [];
+    return {
+      showDropdown: true,
+      isCourseModalOpen: false,
+      courses,
+      emGreen,
+    };
+  },
+  components: {
+    AddCourseButton,
+    NewCourseModal,
+    DropDownArrow,
+    AllRequirementsDropdown,
+    Course,
+  },
+  computed: {
+    requirementChoice(): string {
+      return this.selectedRequirement;
+    },
+  },
+  methods: {
+    openCourseModal() {
+      this.isCourseModalOpen = !this.isCourseModalOpen;
+    },
+    closeCourseModal() {
+      this.isCourseModalOpen = false;
+    },
+    closeDropdownIfOpen() {
+      this.showDropdown = false;
+    },
+    selectRequirement(requirement: string) {
+      if (this.selectedRequirement !== '') {
+        this.$emit('add-available-requirement', this.selectedRequirement);
+      }
+      this.$emit('select-requirement', requirement, this.index);
+      this.$emit('delete-available-requirement', requirement);
+    },
+    deleteRequirement() {
+      this.$emit('delete-requirement');
+    },
+    addCourse(course: CornellCourseRosterCourse) {
+      this.courses.push(
+        cornellCourseRosterCourseToFirebaseSemesterCourseWithCustomIDAndColor(
+          course,
+          -1,
+          store.state.subjectColors[course.subject]
+        )
+      );
+    },
+    deleteCourse(code: string) {
+      this.courses = this.courses.filter(course => course.code !== code);
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables.scss';
+.requirement {
+  border-top: 1px solid;
+  border-color: $inactiveGray;
+  margin-bottom: 0.5rem;
+
+  &-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 6px;
+  }
+
+  &-dropdown {
+    padding-bottom: 3px;
+  }
+
+  &-header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 28px;
+    &-text {
+      color: $emGreen;
+      margin-bottom: 0;
+      margin-left: 12px;
+      font-size: 16px;
+      font-weight: 900;
+    }
+    &-subtext {
+      color: $inactiveGray;
+    }
+  }
+  &-courses {
+    margin-top: 1rem;
+    margin-bottom: 1.2rem;
+  }
+  &-courseWrapper {
+    padding: 0.2rem;
+  }
+}
+</style>
--- src/components/ScheduleGenerate/ScheduleGenerateSideBar.vue
+++ src/components/ScheduleGenerate/ScheduleGenerateSideBar.vue
@@ -1,59 +1,104 @@
 <template>
-  <div class="generatorheader">
-    <h1 class="top header-title">Semester Schedule Builder</h1>
-    <div class="semester">
-      <button class="semester-title-button semester-title full-opacity-on-hover" :disabled="true">
-        <p class="semester-title-top">
-          <!-- Example: "Spring 2025", as on Figma. -->
-          {{ selectedSemester }}
-        </p>
-      </button>
-    </div>
-    <!-- NOTE: you cannot generate a schedule if you have not inputted any requirements. -->
-    <button
-      class="generate-schedule-button"
-      :disabled="generateScheduleButtonDisabled"
-      @click="openScheduleGenerateModal"
-    >
-      <!-- Tools SVG icon placed inline with the text. -->
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-      >
-        <path
-          d="M5.33 3.271C5.99704 3.03298 6.72059 3.00293 7.40506 3.1848C8.08954 3.36668 8.70274 3.75193 9.16365 4.28966C9.62456 4.82739 9.91149 5.49229 9.98654 6.19653C10.0616 6.90077 9.92123 7.61121 9.584 8.234L20.293 18.944L18.879 20.358L8.169 9.648C7.54607 9.9839 6.83599 10.1232 6.13233 10.0475C5.42867 9.97181 4.76445 9.6847 4.22721 9.22401C3.68997 8.76332 3.30492 8.15066 3.1228 7.46677C2.94067 6.78289 2.97003 6.05987 3.207 5.393L5.444 7.63C5.58237 7.77327 5.74789 7.88754 5.93089 7.96615C6.1139 8.04477 6.31073 8.08614 6.5099 8.08788C6.70907 8.08961 6.90659 8.05165 7.09093 7.97623C7.27528 7.90081 7.44275 7.78943 7.58359 7.64859C7.72443 7.50775 7.83581 7.34028 7.91123 7.15593C7.98665 6.97159 8.02461 6.77407 8.02288 6.5749C8.02114 6.37573 7.97976 6.1789 7.90115 5.99589C7.82254 5.81289 7.70826 5.64737 7.565 5.509L5.329 3.27L5.33 3.271ZM15.697 5.155L18.879 3.387L20.293 4.801L18.525 7.983L16.757 8.337L14.637 10.458L13.222 9.044L15.343 6.923L15.697 5.155ZM8.979 13.287L10.393 14.701L5.09 20.004C4.9097 20.1848 4.66706 20.2898 4.41183 20.2974C4.1566 20.3051 3.90811 20.2148 3.71732 20.0451C3.52652 19.8754 3.40786 19.6392 3.38568 19.3848C3.36349 19.1304 3.43946 18.8772 3.598 18.677L3.676 18.59L8.979 13.287Z"
-          fill="white"
-        />
-      </svg>
-      Generate Schedule
-    </button>
-    <div class="credit-limit-container">
-      <div>
-        <label class="credit-limit-label">Credit Limit</label>
-        <!--
-          NOTE: min and max are only semantic here.
-          
-          That is to say, they would only be enforced using the up and down arrows, which have been purposefully removed for aesthetic reasons.
-
-          As such, if we want to enforce the credit limit here, would have to implement some sort of reactive JavaScript listener.
-        -->
-        <input type="number" placeholder='"18"' min="0" max="30" class="credit-limit-input" />
+  <div class="sidebar">
+    <confirmation :text="confirmationText" v-if="isConfirmationOpen" />
+    <div class="sidebar-header">
+      <h1 class="top header-title">Semester Schedule Builder</h1>
+      <div class="semester">
+        <button class="semester-title-button semester-title full-opacity-on-hover" :disabled="true">
+          <p class="semester-title-top">
+            <!-- Example: "Spring 2025", as on Figma. -->
+            {{ selectedSemester }}
+          </p>
+        </button>
       </div>
-      <button class="add-requirement-button">+ Requirement</button>
+      <!-- NOTE: you cannot generate a schedule if you have not inputted any requirements. -->
+      <button
+        class="generate-schedule-button"
+        :disabled="generateScheduleButtonDisabled"
+        @click="openScheduleGenerateModal"
+      >
+        <!-- Tools SVG icon placed inline with the text. -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M5.33 3.271C5.99704 3.03298 6.72059 3.00293 7.40506 3.1848C8.08954 3.36668 8.70274 3.75193 9.16365 4.28966C9.62456 4.82739 9.91149 5.49229 9.98654 6.19653C10.0616 6.90077 9.92123 7.61121 9.584 8.234L20.293 18.944L18.879 20.358L8.169 9.648C7.54607 9.9839 6.83599 10.1232 6.13233 10.0475C5.42867 9.97181 4.76445 9.6847 4.22721 9.22401C3.68997 8.76332 3.30492 8.15066 3.1228 7.46677C2.94067 6.78289 2.97003 6.05987 3.207 5.393L5.444 7.63C5.58237 7.77327 5.74789 7.88754 5.93089 7.96615C6.1139 8.04477 6.31073 8.08614 6.5099 8.08788C6.70907 8.08961 6.90659 8.05165 7.09093 7.97623C7.27528 7.90081 7.44275 7.78943 7.58359 7.64859C7.72443 7.50775 7.83581 7.34028 7.91123 7.15593C7.98665 6.97159 8.02461 6.77407 8.02288 6.5749C8.02114 6.37573 7.97976 6.1789 7.90115 5.99589C7.82254 5.81289 7.70826 5.64737 7.565 5.509L5.329 3.27L5.33 3.271ZM15.697 5.155L18.879 3.387L20.293 4.801L18.525 7.983L16.757 8.337L14.637 10.458L13.222 9.044L15.343 6.923L15.697 5.155ZM8.979 13.287L10.393 14.701L5.09 20.004C4.9097 20.1848 4.66706 20.2898 4.41183 20.2974C4.1566 20.3051 3.90811 20.2148 3.71732 20.0451C3.52652 19.8754 3.40786 19.6392 3.38568 19.3848C3.36349 19.1304 3.43946 18.8772 3.598 18.677L3.676 18.59L8.979 13.287Z"
+            fill="white"
+          />
+        </svg>
+        Generate Schedule
+      </button>
+      <div class="credit-limit-container">
+        <div>
+          <label class="credit-limit-label">Credit Limit</label>
+          <!--
+            NOTE: min and max are only semantic here.
+            
+            That is to say, they would only be enforced using the up and down arrows, which have been purposefully removed for aesthetic reasons.
+
+            As such, if we want to enforce the credit limit here, would have to implement some sort of reactive JavaScript listener.
+          -->
+          <input type="number" placeholder='"18"' min="0" max="30" class="credit-limit-input" />
+        </div>
+        <button
+          class="add-requirement-button"
+          @click="addRequirement"
+          :disabled="requirements.length === numberOfRequirements"
+        >
+          + Requirement
+        </button>
+      </div>
     </div>
-    <hr />
     <!-- The Figma wrote the below as 'No Requirements added' but I believe that was a capitalization mistake. -->
-    <p class="no-requirements-added">No requirements added.</p>
+    <p v-if="requirements.length === 0" class="no-requirements-added">No requirements added.</p>
+    <div v-for="(req, index) in requirements" :key="req">
+      <requirement-courses
+        :availableRequirements="availableRequirements"
+        :selected-requirement="requirements[index]"
+        :index="index"
+        @select-requirement="selectRequirement"
+        @delete-available-requirement="deleteAvailableRequirement"
+        @add-available-requirement="addAvailableRequirement"
+        @delete-requirement="deleteRequirement(index)"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import RequirementCourses from '@/components/ScheduleGenerate/RequirementCourses.vue';
+import Confirmation from '@/components/Modals/Confirmation.vue';
 
 export default defineComponent({
+  data(): {
+    requirements: string[];
+    availableRequirements: Record<string, string>;
+    numberOfRequirements: number;
+    isConfirmationOpen: boolean;
+    confirmationText: string;
+  } {
+    return {
+      requirements: [],
+      // TODO: change hard coded requirements to requirement ids.
+      availableRequirements: {
+        'CS Requirement': 'CS Requirement',
+        'Probability Requirement': 'Probability Requirement',
+        'Liberal Studies': 'Liberal Studies',
+      },
+      numberOfRequirements: 3,
+      isConfirmationOpen: false,
+      confirmationText: '',
+    };
+  },
+  components: {
+    RequirementCourses,
+    Confirmation,
+  },
   props: {
     // current semester being generated for
     selectedSemester: { type: String, required: true },
@@ -62,6 +107,41 @@ export default defineComponent({
   },
   emits: ['openScheduleGenerateModal'],
   methods: {
+    openConfirmationModal(msg: string) {
+      // Set text and display confirmation modal, then have it disappear after 3 seconds
+      this.confirmationText = msg;
+      this.isConfirmationOpen = true;
+
+      setTimeout(() => {
+        this.closeConfirmationModal();
+      }, 2000);
+    },
+    closeConfirmationModal() {
+      this.isConfirmationOpen = false;
+    },
+    addRequirement() {
+      this.requirements = [...this.requirements, ''];
+    },
+    deleteAvailableRequirement(requirement: string) {
+      delete this.availableRequirements[requirement];
+    },
+    selectRequirement(requirement: string, index: number) {
+      this.requirements[index] = requirement;
+    },
+    addAvailableRequirement(requirement: string) {
+      if (requirement !== '') this.availableRequirements[requirement] = requirement;
+    },
+    deleteRequirement(index: number) {
+      const requirement = this.requirements[index];
+      // add back to the availableRequirements record
+      this.addAvailableRequirement(requirement);
+
+      // delete this requirement from list
+      this.requirements.splice(index, 1);
+      this.openConfirmationModal(
+        `Removed ${requirement === '' ? 'requirement' : requirement} from schedule builder`
+      );
+    },
     openScheduleGenerateModal() {
       this.$emit('openScheduleGenerateModal');
     },
@@ -72,10 +152,13 @@ export default defineComponent({
 <style scoped lang="scss">
 @import '@/assets/scss/_variables.scss';
 
-.generatorheader {
+.sidebar {
   width: 25rem;
   background-color: $white;
   padding: 1.625rem 1.5rem;
+  &-header {
+    margin-bottom: 1rem;
+  }
 }
 
 /*
@@ -247,5 +330,11 @@ export default defineComponent({
   border-radius: 4px;
   font-size: 16px;
   align-items: center;
+}
+
+.add-requirement-button:disabled {
+  border: 1px solid $inactiveGray;
+  background: $inactiveGray;
+  cursor: not-allowed;
 }
 </style>
