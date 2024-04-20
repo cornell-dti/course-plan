d4594011b8d382b5a912d04c8abbefc8842f9189
Nidhi Mylavarapu <73746818+nidhi-mylavarapu@users.noreply.github.com>
2024-04-20 17:21:48 -0400

    Requirement Migrations (#874)
    * Began working on migration structure with ECE core course requirements
    
    * formatting
    
    * All updates with requirement migrations but error with fieldOfStudyReqs function type
    
    * JSON Updated with decorated migrations - still need to change json generator to include all fields of migrations
    
    * Working requirement migrations with ECE core courses migrations
    
    * formatting
    
    * Maintained order of requirements when migrating, fixed format
    
    * Added helper
    
    * Added more inline comments

--- src/data/index.ts
+++ src/data/index.ts
@@ -25,7 +25,7 @@ import csRequirements, { csAdvisors } from './majors/cs';
 import deaRequirements, { deaAdvisors } from './majors/dea';
 import easRequirements, { easAdvisors } from './majors/eas';
 import economicsRequirements, { economicsAdvisors } from './majors/econ';
-import eceRequirements, { eceAdvisors } from './majors/ece';
+import eceRequirements, { eceAdvisors, eceMigrations } from './majors/ece';
 import essRequirements, { essAdvisors } from './majors/ess';
 import englishRequirements, { englishAdvisors } from './majors/engl';
 import envEngineeringRequirements, { envEngineeringAdvisors } from './majors/envE';
@@ -266,6 +266,7 @@ const json: RequirementsJson = {
       schools: ['EN'],
       requirements: eceRequirements,
       advisors: eceAdvisors,
+      migrations: eceMigrations,
       abbrev: 'ECE',
     },
     ENGL: {
