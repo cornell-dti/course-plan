d37b573d45cb56c687c5d800524558df023b3a48
Nidhi Mylavarapu <nidhi.mylavarapu@gmail.com>
2024-04-22 10:38:35 -0400

    courses from user input show up on modal, schedule info passed into modal for gen request

--- src/components/ScheduleGenerate/ScheduleGenerateModal.vue
+++ src/components/ScheduleGenerate/ScheduleGenerateModal.vue
@@ -146,7 +146,6 @@ export default defineComponent({
       console.log('From Schedule Generate MODAL REQS SELECTED');
       console.log(this.reqIds);
       return returnCourses;
-      // added change
     },
     classesSchedule() {
       return {
