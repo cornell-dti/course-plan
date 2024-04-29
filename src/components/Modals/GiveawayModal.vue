35dace4a575218a947d394c9920ab39cdbbca0fa
elizabeth-tang <eliza.n.tang@gmail.com>
2024-04-29 18:06:26 -0400

    new files

---
+++ src/components/Modals/GiveawayModal.vue
@@ -0,0 +1,254 @@
+<template>
+  <div
+    class="teleport"
+    :class="{
+      'teleport-noBackground': hasNoBackground,
+      'teleport-transparentBackground': hasClickableTransparentBackground,
+    }"
+    @click="closeOnClickOutside"
+    ref="modalBackground"
+  >
+    <div :class="['modal-content', contentClass]">
+      <component :is="'script'" src="@/assets/confetti.js"></component>
+      <button @click="startConfetti()">hello</button>
+      <div class="modal-top">
+        <button @click="close" class="modal-exitbutton" data-cyId="modal-exit">
+          <img class="modal-exit" src="@/assets/images/x.png" alt="x to close modal" />
+        </button>
+      </div>
+      <div class="modal-textWrapper">
+        <img class="modal-logo" src="@/assets/images/branding/logo.svg" />
+        <h1 class="modal-title">{{ title }}</h1>
+        <div>Sign up for your chance to win a $25 gift card!</div>
+        <div class="textInput">
+          <label class="textInput-label">Cornell NetID</label>
+          <div class="textInput-wrapper">
+            <input class="textInput-userinput" placeholder="Enter netid" v-model="netId" />
+          </div>
+          <label class="textInput-label">Instagram Username</label>
+          <div class="textInput-wrapper">
+            <input class="textInput-userinput" placeholder="Enter username" v-model="igUsername" />
+          </div>
+        </div>
+      </div>
+      <div class="modal-buttonWrapper">
+        <button
+          class="modal-button modal-button--add"
+          :class="{
+            'modal-button--highlighted': rightButtonIsHighlighted,
+          }"
+          @click="submitEntry"
+        >
+          <img
+            v-if="rightButtonImage"
+            class="modal-icon"
+            :src="rightButtonImage"
+            :alt="rightButtonAlt"
+          />
+          {{ rightButtonText }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import enterGiveaway from '@/global-firestore-data/giveaway-entries';
+
+export default defineComponent({
+  props: {
+    title: { type: String, default: '' },
+    contentClass: { type: String, required: true },
+    rightButtonText: { type: String, default: '' },
+    rightButtonImage: { type: String, default: '' },
+    rightButtonAlt: { type: String, default: '' },
+    rightButtonIsHighlighted: { type: Boolean, default: false },
+    hasNoBackground: { type: Boolean, default: false }, // true for modals without the gray overlay behind them
+    hasClickableTransparentBackground: { type: Boolean, default: false }, // modals without a gray overlay behind them AND clicking on the background closes the modal
+  },
+  emits: ['left-button-clicked', 'right-button-clicked', 'modal-closed'],
+  data() {
+    return { netId: '', igUsername: '' };
+  },
+  methods: {
+    submitEntry() {
+      if (this.netId !== '' && this.igUsername !== '') {
+        enterGiveaway(this.netId, this.igUsername);
+        this.$emit('modal-closed', true);
+      }
+      console.log(this.netId);
+      console.log(this.igUsername);
+    },
+  },
+  setup(props, { emit }) {
+    const modalBackground = ref((null as unknown) as HTMLDivElement);
+
+    const close = () => {
+      emit('modal-closed', true);
+    };
+
+    const closeOnClickOutside = (e: MouseEvent) => {
+      if (e.target === modalBackground.value) close();
+    };
+
+    const leftButtonClicked = () => {
+      emit('left-button-clicked');
+    };
+
+    const rightButtonClicked = () => {
+      emit('right-button-clicked');
+    };
+
+    return { close, closeOnClickOutside, leftButtonClicked, rightButtonClicked, modalBackground };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables.scss';
+
+.teleport {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  min-height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 4;
+  padding: 1rem;
+
+  &-noBackground {
+    background-color: rgba(0, 0, 0, 0);
+    width: 100%;
+    min-height: 0;
+    pointer-events: none;
+  }
+
+  &-transparentBackground {
+    background-color: rgba(0, 0, 0, 0);
+    width: 100%;
+    min-height: 100vh;
+  }
+}
+
+.modal {
+  &-content {
+    width: 24rem;
+    background: $white;
+    border-radius: 9px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 1rem;
+  }
+  &-logo {
+    margin-bottom: 1rem;
+  }
+  &-textWrapper {
+    text-align: center;
+  }
+  &-body {
+    padding: 0;
+  }
+
+  &-top {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+  }
+  &-exitbutton {
+    margin-left: auto;
+  }
+  &-exit {
+    width: 10.5px;
+    height: 10.5px;
+  }
+
+  &-icon {
+    margin-right: 0.25rem;
+  }
+
+  &-buttonWrapper {
+    margin-top: 1rem;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  &-button {
+    width: 4.75rem;
+    height: 1.8rem;
+    color: $sangBlue;
+    border-radius: 3px;
+    border: 1px solid $sangBlue;
+    background-color: $white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &--add {
+      color: $white;
+      background-color: $sangBlue;
+      margin-left: 0.5rem;
+      border: none;
+    }
+
+    &--highlighted {
+      border: 2px solid $error;
+    }
+
+    &--big {
+      width: 7rem;
+    }
+  }
+}
+
+.textInput {
+  width: 20rem;
+  margin: auto;
+  margin-top: 0.5rem;
+  &-label {
+    font-weight: bold;
+    top: 0.5rem;
+    position: relative;
+    float: left;
+  }
+  &-wrapper {
+    height: 4.2rem;
+    width: 20rem;
+    margin: auto;
+  }
+  &-userinput {
+    border: 1px solid $darkPlaceholderGray;
+    top: 0.5rem;
+    position: relative;
+    width: 100%;
+    height: 2rem;
+    padding-left: 0.5rem;
+    border-radius: 3px;
+    &::placeholder {
+      color: $darkPlaceholderGray;
+    }
+  }
+}
+
+// custom styling for different modals depending on contentClass
+.content {
+  &-confirmation {
+    border: none;
+    margin-top: 1rem;
+    min-height: 0;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    align-items: center;
+    pointer-events: none;
+  }
+
+  &-slotmenu {
+    background-color: $white;
+    min-height: 0;
+    width: 7rem;
+    position: fixed;
+  }
+}
+</style>
