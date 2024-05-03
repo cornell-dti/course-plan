1e2141a193ba02e67a56ca60a6f9d3ed2595171a
Simon Ilincev <trilogicworlds@gmail.com>
2024-05-02 22:46:51 -0400

    docs: update readme based on algorithm updates

--- src/schedule-generator/course-unit.ts
+++ src/schedule-generator/course-unit.ts
@@ -36,15 +36,6 @@ export default class Course {
   /*
   *all* of these have to be available for the course to be scheduled.
   will usually just be one, but if there is e.g. a lab or a discussion then it could be two
-  for now, just going to have multiple copies of the course.
-  
-  say we have one lecture L and two discussions D1 and D2.
-  then there would be generated:
-
-  Course(code=L, timeslots=[LectureTimeslot, D1Timeslot])
-  Course(code=L, timeslots=[LectureTimeslot, D2Timeslot])
-
-  then the algorithm will just choose one of these to schedule
   */
   timeslots: Timeslot[];
 
