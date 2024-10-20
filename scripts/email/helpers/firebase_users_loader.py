d4c554a9b310199ce616c6af840ab0b6521c4f5f
Simon Ilincev <trilogicworlds@gmail.com>
2024-10-20 00:15:31 -0400

    chore: comment out currently-unused user name data

--- scripts/email/helpers/firebase_users_loader.py
+++ scripts/email/helpers/firebase_users_loader.py
@@ -9,37 +9,40 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-def get_all_user_names() -> Dict[str, Tuple[str, str, str]]:
-    print("Fetching all user names from Firebase...", end="\n\n")
-    user_names = {}
-    docs = db.collection("user-name").get()
-    for doc in docs:
-        data = doc.to_dict()
-        user_names[doc.id] = (
-            data.get("firstName", None),
-            data.get("middleName", None),
-            data.get("lastName", None),
-        )
-    return user_names
+# NOTE: currently not being used for personalization due to BCC logic.
+# If we do end up personalizing sometime, then uncomment this and associated
+# calls later in the file + update the email script.
+# def get_all_user_names() -> Dict[str, Tuple[str, str, str]]:
+#     print("Fetching all user names from Firebase...", end="\n\n")
+#     user_names = {}
+#     docs = db.collection("user-name").get()
+#     for doc in docs:
+#         data = doc.to_dict()
+#         user_names[doc.id] = (
+#             data.get("firstName", None),
+#             data.get("middleName", None),
+#             data.get("lastName", None),
+#         )
+#     return user_names
 
 
 def get_users() -> Dict[Tuple[str, str], List[Dict]]:
     print("Fetching all user data from Firebase...")
-    user_names = get_all_user_names()
+    # user_names = get_all_user_names()
     users = db.collection("user-onboarding-data").get()
     user_map = defaultdict(list)
 
     for user in users:
         user_data = user.to_dict()
         email = user.id
-        first_name, middle_name, last_name = user_names.get(
-            email, (None, None, None)
-        )  # NOTE: may cause type errors if we don't have a name for this user and try
-        # to send them an email regardless. (Deliberate decision.)
+        # first_name, middle_name, last_name = user_names.get(
+        #     email, (None, None, None)
+        # )  # NOTE: may cause type errors if we don't have a name for this user and try
+        # # to send them an email regardless. (Deliberate decision.)
 
-        # Deleted accounts or something? Not sure to do with these people.
-        if first_name is None and middle_name is None and last_name is None:
-            print(f"User {email} not found in user-name collection!")
+        # # Deleted accounts or something? Not sure to do with these people.
+        # if first_name is None and middle_name is None and last_name is None:
+        #     print(f"User {email} not found in user-name collection!")
 
         colleges = [
             college.get("acronym", "") for college in user_data.get("colleges", [])
@@ -56,9 +59,9 @@ def get_users() -> Dict[Tuple[str, str], List[Dict]]:
         user_map[key].append(
             {
                 "email": email,
-                "first_name": first_name,
-                "middle_name": middle_name,
-                "last_name": last_name,
+                # "first_name": first_name,
+                # "middle_name": middle_name,
+                # "last_name": last_name,
                 "colleges": colleges,
                 "grad_programs": grad_programs,
                 "entrance_semester": entrance_sem,
