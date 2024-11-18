7d0486fdbe65fbfe18f8c03e8e52b015b380e6d9
Eric Cheng <ericcheng9316@gmail.com>
2024-11-18 02:54:02 -0500

    Revert "chore: rerun req gen"
    This reverts commit 311fd9c683eb67e6810ad5f503f1fb40f793ac9f.

--- src/data/majors/cs.ts
+++ src/data/majors/cs.ts
@@ -72,8 +72,7 @@ const csRequirements: readonly CollegeOrMajorRequirement[] = [
         );
       },
     ],
-    checkerWarning:
-      'We do not check whether you are allowed to take CS 3700/3780 towards this requirement.',
+    checkerWarning: 'We do not check whether you are allowed to take CS 3700/3780 towards this requirement.',
     fulfilledBy: 'courses',
     perSlotMinCount: [2],
     slotNames: ['Course'],
@@ -217,13 +216,7 @@ export const csMigrations: RequirementMigration[] = [
       checker: [
         (course: Course): boolean => {
           if (
-            courseMatchesCodeOptions(course, [
-              'CS 4090',
-              'CS 4998',
-              'CS 4999',
-              'CS 4410',
-              'CS 4820',
-            ])
+            courseMatchesCodeOptions(course, ['CS 4090', 'CS 4998', 'CS 4999', 'CS 4410', 'CS 4820'])
           ) {
             return false;
           }
